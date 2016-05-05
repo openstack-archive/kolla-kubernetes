ef15d65ae667588d37cf466de89d96f8ce54c5f0
Steven Dake <stdake@cisco.com>
2016-05-05 02:47:52 -0700

    Test of kubernetes gate
    Change-Id: I8b68fc56ab499f29c2be49495c9f1a8f1eb4086c

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
 # Copyright (c) 2013 Hewlett-Packard Development Company, L.P.
+# comment
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
