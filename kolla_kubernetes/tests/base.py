593ae1c65d13248aa0c4e28590731d6003f543df
Davanum Srinivas <davanum@gmail.com>
2016-05-05 13:39:10 -0400

    Port some tests from Kolla repo
    In tox.ini, specify the git url for kolla repo so that
    we can use code from kolla repo directly (instead of
    specifying a fixed released version of kolla in requirements)
    
    To make sure we don't break this, copied over some tests
    from kolla repo.
    
    Change-Id: I5920ed01a7c43cf4b5f70ee5bc0e6fb4614b0028

--- kolla_kubernetes/tests/base.py
+++ kolla_kubernetes/tests/base.py
@@ -15,9 +15,35 @@
 # License for the specific language governing permissions and limitations
 # under the License.
 
-from oslotest import base
+import os
+
+import fixtures
+import mock
+from oslo_config import cfg
+from oslotest import base as oslotest_base
+
+from kolla.common import config as common_config
 
 
-class TestCase(base.BaseTestCase):
+TESTS_ROOT = os.path.dirname(os.path.abspath(__file__))
 
-    """Test case base class for all unit tests."""
+
+class TestCase(oslotest_base.BaseTestCase):
+    '''All unit test should inherit from this class'''
+    config_file = None
+
+    def setUp(self):
+        super(TestCase, self).setUp()
+        self.conf = cfg.ConfigOpts()
+        default_config_files = self.get_default_config_files()
+        common_config.parse(self.conf, [],
+                            default_config_files=default_config_files)
+        # NOTE(jeffrey4l): mock the _get_image_dir method to return a fake
+        # docker images dir
+        self.useFixture(fixtures.MockPatch(
+            'kolla.cmd.build.KollaWorker._get_images_dir',
+            mock.Mock(return_value=os.path.join(TESTS_ROOT, 'docker'))))
+
+    def get_default_config_files(self):
+        if self.config_file:
+            return [os.path.join(TESTS_ROOT, 'etc', self.config_file)]
