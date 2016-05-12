11de35a07ad57a259caef7fd49a3e7eef97f496e
Davanum Srinivas <davanum@gmail.com>
2016-05-12 17:12:24 -0400

    Major Cleanup - Get command line working
    * Remove Dead code
    * Deal better with various directories
    * Add logging for missing stuff
    
    Partially-implements: blueprint kolla-kubernetes-cli
    Change-Id: Ia9f6b406c54f3f561ae360a708966e8fa49fb396

--- kolla_kubernetes/common/utils.py
+++ kolla_kubernetes/common/utils.py
@@ -10,9 +10,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import collections
 import os
-from six.moves.urllib import parse
 
 
 def env(*args, **kwargs):
@@ -23,24 +21,31 @@ def env(*args, **kwargs):
     return kwargs.get('default', '')
 
 
-def dict_update(d, u):
-    """Recursively update 'd' with 'u' and return the result."""
+class JvarsDict(dict):
+    """Dict which can contain the 'global_vars' which are always preserved.
 
-    if not isinstance(u, collections.Mapping):
-        return u
+    They cannot be be overriden by any update nor single item setting.
+    """
 
-    for k, v in u.items():
-        if isinstance(v, collections.Mapping):
-            d[k] = dict_update(d.get(k, {}), v)
-        else:
-            d[k] = u[k]
-    return d
+    def __init__(self, *args, **kwargs):
+        super(JvarsDict, self).__init__(*args, **kwargs)
+        self.global_vars = {}
 
+    def __setitem__(self, key, value, force=False):
+        if not force and key in self.global_vars:
+            return
+        return super(JvarsDict, self).__setitem__(key, value)
 
-def get_query_string(search_opts):
-    if search_opts:
-        qparams = sorted(search_opts.items(), key=lambda x: x[0])
-        query_string = "?%s" % parse.urlencode(qparams, doseq=True)
-    else:
-        query_string = ""
-    return query_string
+    def set_force(self, key, value):
+        """Sets the variable even if it will override a global variable."""
+        return self.__setitem__(key, value, force=True)
+
+    def update(self, other_dict, force=False):
+        if not force:
+            other_dict = {key: value for key, value in other_dict.items()
+                          if key not in self.global_vars}
+        super(JvarsDict, self).update(other_dict)
+
+    def set_global_vars(self, global_vars):
+        self.update(global_vars)
+        self.global_vars = global_vars
