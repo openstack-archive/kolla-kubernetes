359a43203157cb49224236f1cb16cfe4d65cc3b8
Davanum Srinivas <davanum@gmail.com>
2016-05-12 17:12:24 -0400

    Major Cleanup - Get command line working
    * Remove Dead code
    * Deal better with various directories
    * Add logging for missing stuff
    
    Change-Id: Ia9f6b406c54f3f561ae360a708966e8fa49fb396

--- kolla_kubernetes/common/file_utils.py
+++ kolla_kubernetes/common/file_utils.py
@@ -10,11 +10,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import errno
 import logging
 import os
-import platform
-import sys
 
 from oslo_utils import importutils
 
@@ -24,29 +21,19 @@ from kolla_kubernetes import exception
 LOG = logging.getLogger(__name__)
 
 
-def find_os_type():
-    return platform.linux_distribution()[0]
-
-
-def mkdir_p(path):
-    try:
-        os.makedirs(path)
-    except OSError as exc:  # Python >2.5
-        if exc.errno == errno.EEXIST and os.path.isdir(path):
-            pass
-        else:
-            raise
-
-
-def get_services_dir(base_dir):
-    if os.path.exists(os.path.join(base_dir, 'services')):
-        return os.path.join(base_dir, 'services')
-    elif os.path.exists(os.path.join(get_src_dir(), 'services')):
-        return os.path.join(get_src_dir(), 'services')
+def find_config_file(filename):
+    filepath = os.path.join('/etc/kolla', filename)
+    if os.access(filepath, os.R_OK):
+        return filepath
     raise exception.KollaDirNotFoundException(
         'Unable to detect kolla-kubernetes directory'
     )
 
+def get_service_config_files(service):
+    directory = os.path.join('/etc/kolla/', service)
+    for dirpath, _, filenames in os.walk(directory):
+        for f in filenames:
+            yield os.path.abspath(os.path.join(dirpath, f))
 
 def get_src_dir():
     kolla_kubernetes = importutils.import_module('kolla_kubernetes')
@@ -54,56 +41,19 @@ def get_src_dir():
     # remove the file and module to get to the base.
     return os.path.dirname(os.path.dirname(mod_path))
 
-
 def get_shared_directory():
     if os.path.exists('/usr/local/share/kolla'):
         return '/usr/local/share/kolla'
     elif os.path.exists('/usr/share/kolla'):
         return '/usr/share/kolla'
-    return None
-
-
-def find_base_dir():
-    script_path = os.path.dirname(os.path.realpath(sys.argv[0]))
-    base_script_path = os.path.basename(script_path)
-    if base_script_path == 'kolla-kubernetes':
-        return script_path
-    if base_script_path == 'kolla_kubernetes':
-        return os.path.join(script_path, '..')
-    if base_script_path == 'cmd':
-        return os.path.join(script_path, '..', '..')
-    if base_script_path == 'subunit':
-        return get_src_dir()
-    if base_script_path == 'bin':
-        if os.path.exists('/usr/local/share/kolla'):
-            return '/usr/local/share/kolla'
-        elif os.path.exists('/usr/share/kolla'):
-            return '/usr/share/kolla'
-        else:
-            return get_src_dir()
     raise exception.KollaDirNotFoundException(
         'Unable to detect kolla-kubernetes directory'
     )
 
-
-def find_config_file(filename):
-    filepath = os.path.join('/etc/kolla-kubernetes', filename)
-    if os.access(filepath, os.R_OK):
-        config_file = filepath
+def find_base_dir():
+    if os.path.exists('/usr/local/share/kolla'):
+        return '/usr/local/share/kolla'
+    elif os.path.exists('/usr/share/kolla'):
+        return '/usr/share/kolla'
     else:
-        config_file = os.path.join(find_base_dir(),
-                                   'etc', filename)
-    return config_file
-
-
-POSSIBLE_PATHS = {'/usr/share/kolla-kubernetes',
-                  get_src_dir(),
-                  find_base_dir()}
-
-
-def find_file(filename):
-    for path in POSSIBLE_PATHS:
-        file_path = os.path.join(path, filename)
-        if os.path.exists(file_path):
-            return file_path
-    raise exception.KollaNotFoundException(filename, entity='file')
+        return get_src_dir()
