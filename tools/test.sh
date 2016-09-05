2f02c1a10c49baf6214a0c99eaeb961a3792fa44
Kevin Fox <kevin@efox.cc>
2016-09-05 09:01:37 -0700

    Template validation/gating
    This adds support to the tests/gate for checking if all the
    templates are valid yaml. If they have alpha init-containers,
    check if they are valid json.
    
    It also fixes a few broken links in the service catalog that
    the tests discovered were broken.
    
    Change-Id: I8149f68437b4604fc79ac7f1e5a2811f9a706dfa

---
+++ tools/test.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+[ ! -d ../kolla ] && pushd .. && git clone https://github.com/openstack/kolla && popd
+grep api_interface_address ../kolla/etc/kolla/globals.yml || echo api_interface_address: "0.0.0.0" >> ../kolla/etc/kolla/globals.yml
+python setup.py test --slowest --testr-args="$1"
