60cc83bbdcdb0dacda599187deb3f1c0a755c853
Kevin Fox <kevin@efox.cc>
2016-09-05 17:04:21 -0700

    Checks to start reducing Technical Debt
    We already have a lot of technical debt. This test adds checks for 3
    of the types we currently have. There will be follow up patches for
    adding tests for more of the types later. The tests this implements
    are:
    1. Not having explicit namespaces on namespaced resources.
    2. Having namespaces in the template description. Its implied by the
       namespace in the deployment/petset/daemonset/replicationcontroller
    3. Not having a nodeSelector on a resource.
    
    It also white lists all existing occurrences so they can be picked
    off in followup patches and tested for correctness.
    
    Change-Id: I316f8c3edf24d33156e8effc23a825c15a158cdb

--- kolla_kubernetes/tests/test_templates.py
+++ kolla_kubernetes/tests/test_templates.py
@@ -20,6 +20,125 @@ from kolla_kubernetes.tests import base
 KKR = KollaKubernetesResources.Get()
 RESOURCE_TYPES = ('secret pv pvc svc bootstrap pod').split(" ")
 
+technical_debt = {
+    'nodeSelector': [
+        ['pod', 'nova', 'nova-compute-pod'],
+        ['pod', 'openvswitch', 'openvswitch-db'],
+        ['pod', 'openvswitch', 'openvswitch-vswitchd'],
+        ['pod', 'neutron', 'neutron-openvswitch-agent-daemonset'],
+        ['bootstrap', 'mariadb', 'mariadb-bootstrap-job'],
+        ['pod', 'mariadb', 'mariadb-pod'],
+        ['pod', 'memcached', 'memcached-pod'],
+        ['bootstrap', 'keystone', 'keystone-bootstrap-job'],
+        ['pod', 'keystone', 'keystone-pod'],
+        ['pod', 'horizon', 'horizon-pod'],
+        ['bootstrap', 'rabbitmq', 'rabbitmq-bootstrap-job'],
+        ['pod', 'rabbitmq', 'rabbitmq-pod'],
+        ['bootstrap', 'glance', 'glance-bootstrap-job'],
+        ['pod', 'glance', 'glance-api-pod'],
+        ['pod', 'glance', 'glance-registry-pod'],
+        ['bootstrap', 'nova', 'nova-compute-bootstrap-job'],
+        ['bootstrap', 'nova', 'create-api-db'],
+        ['bootstrap', 'nova', 'create-endpoints'],
+        ['bootstrap', 'nova', 'create-db'],
+        ['pod', 'nova', 'nova-api-pod'],
+        ['pod', 'nova', 'nova-conductor-pod'],
+        ['pod', 'nova', 'nova-scheduler-pod'],
+        ['bootstrap', 'neutron', 'neutron-bootstrap-job'],
+        ['pod', 'neutron', 'neutron-control-pod'],
+        ['pod', 'neutron', 'neutron-dhcp-agent-pod'],
+        ['pod', 'neutron', 'neutron-l3-agent-pod'],
+        ['pod', 'neutron', 'neutron-metadata-agent-pod'],
+        ['pod', 'swift', 'swift-account-pod'],
+        ['pod', 'swift', 'swift-container-pod'],
+        ['pod', 'swift', 'swift-object-pod'],
+        ['pod', 'swift', 'swift-proxy-pod'],
+        ['pod', 'skydns', 'skydns-pod'],
+        ['bootstrap', 'cinder',
+         'cinder-bootstrap-job-create-db'],
+        ['bootstrap', 'cinder',
+         'cinder-bootstrap-job-manage-db'],
+        ['bootstrap', 'cinder',
+         'cinder-bootstrap-job-endpoints'],
+        ['pod', 'cinder', 'cinder-api-pod'],
+        ['pod', 'cinder', 'cinder-scheduler-pod']
+    ],
+    'namespaceNotFound': [
+        ['pvc', 'mariadb', 'mariadb-pvc'],
+        ['svc', 'mariadb', 'mariadb-service'],
+        ['svc', 'memcached', 'memcached-service'],
+        ['svc', 'keystone', 'keystone-service-admin'],
+        ['svc', 'keystone', 'keystone-service-public'],
+        ['svc', 'horizon', 'horizon-service'],
+        ['pvc', 'rabbitmq', 'rabbitmq-pvc'],
+        ['svc', 'rabbitmq', 'rabbitmq-service-management'],
+        ['svc', 'rabbitmq', 'rabbitmq-service'],
+        ['pvc', 'glance', 'glance-pvc'],
+        ['svc', 'glance', 'glance-api-service'],
+        ['svc', 'glance', 'glance-registry-service'],
+        ['pod', 'glance', 'glance-api-haproxy-configmap'],
+        ['pod', 'glance', 'glance-registry-haproxy-configmap'],
+        ['svc', 'nova', 'nova-api'],
+        ['svc', 'nova', 'nova-metadata'],
+        ['pod', 'nova', 'nova-compute-pod'],
+        ['svc', 'neutron', 'neutron-server-service'],
+        ['pod', 'neutron', 'neutron-openvswitch-agent-daemonset'],
+        ['svc', 'swift', 'swift-account-service'],
+        ['svc', 'swift', 'swift-container-service'],
+        ['svc', 'swift', 'swift-object-service'],
+        ['svc', 'swift', 'swift-proxy-service'],
+        ['svc', 'swift', 'swift-rsync-service'],
+        ['pvc', 'cinder', 'cinder-pvc'],
+        ['svc', 'cinder', 'cinder-api-service'],
+        ['pod', 'cinder', 'cinder-api-haproxy-configmap'],
+        ['bootstrap', 'mariadb', 'mariadb-bootstrap-job'],
+        ['pod', 'mariadb', 'mariadb-pod'],
+        ['pod', 'memcached', 'memcached-pod'],
+        ['bootstrap', 'keystone', 'keystone-bootstrap-job'],
+        ['pod', 'keystone', 'keystone-pod'],
+        ['pod', 'horizon', 'horizon-pod'],
+        ['bootstrap', 'rabbitmq', 'rabbitmq-bootstrap-job'],
+        ['pod', 'rabbitmq', 'rabbitmq-pod'],
+        ['bootstrap', 'glance', 'glance-bootstrap-job'],
+        ['pod', 'glance', 'glance-api-pod'],
+        ['pod', 'glance', 'glance-registry-pod'],
+        ['bootstrap', 'nova', 'nova-compute-bootstrap-job'],
+        ['bootstrap', 'nova', 'create-api-db'],
+        ['bootstrap', 'nova', 'create-endpoints'],
+        ['bootstrap', 'nova', 'create-db'],
+        ['pod', 'nova', 'nova-api-pod'],
+        ['pod', 'nova', 'nova-conductor-pod'],
+        ['pod', 'nova', 'nova-scheduler-pod'],
+        ['bootstrap', 'neutron', 'neutron-bootstrap-job'],
+        ['pod', 'neutron', 'neutron-control-pod'],
+        ['pod', 'neutron', 'neutron-dhcp-agent-pod'],
+        ['pod', 'neutron', 'neutron-l3-agent-pod'],
+        ['pod', 'neutron', 'neutron-metadata-agent-pod'],
+        ['pod', 'swift', 'swift-account-pod'],
+        ['pod', 'swift', 'swift-container-pod'],
+        ['pod', 'swift', 'swift-object-pod'],
+        ['pod', 'swift', 'swift-proxy-pod'],
+        ['bootstrap', 'cinder', 'cinder-bootstrap-job-create-db'],
+        ['bootstrap', 'cinder', 'cinder-bootstrap-job-manage-db'],
+        ['bootstrap', 'cinder', 'cinder-bootstrap-job-endpoints'],
+        ['pod', 'cinder', 'cinder-api-pod'],
+        ['pod', 'cinder', 'cinder-scheduler-pod']
+    ],
+    'namespaceInTemplate': [
+        ['pod', 'nova', 'nova-compute-pod'],
+        ['pod', 'openvswitch', 'openvswitch-db'],
+        ['pod', 'openvswitch', 'openvswitch-vswitchd']
+    ]
+}
+
+
+def unknown_technical_debt(args, selector):
+    for i in technical_debt[selector]:
+        if i[0] == args.resource_type and i[1] == args.service_name and \
+           i[2] == args.resource_name:
+            return False
+    return True
+
 
 class argobj(object):
 
@@ -48,13 +167,16 @@ def on_each_template(func):
                 rt = ResourceTemplate('kolla-kubernetes.py',
                                       '', 'resource-template')
                 o = rt.take_action(args=args, skip_and_return=True)
-                func(argobj, o)
+                func(args, o)
 
 
 class TestTemplatesTest(base.BaseTestCase):
 
     def test_validate_templates(self):
-        def func(argobj, o):
+        WERROR = True
+        WARNING = {'found': False}
+
+        def func(args, o):
             # Check if template is yaml
             y = yaml.load(o)
             js = '[]'
@@ -71,4 +193,23 @@ class TestTemplatesTest(base.BaseTestCase):
                 if e.args[0] not in m:
                     raise
             json.loads(js)
+            if args.service_name != 'ceph':
+                kind = y['kind']
+                if 'namespace' not in y['metadata'] and \
+                   kind != 'PersistentVolume' and \
+                   unknown_technical_debt(args, 'namespaceNotFound'):
+                    raise("namespace not found but required.")
+                if kind in ('PetSet', 'Deployment', 'Job', 'DaemonSet',
+                            'ReplicationController', 'Pod'):
+                    pod = y
+                    if kind != 'Pod':
+                        pod = y['spec']['template']
+                    if 'nodeSelector' not in pod['spec'] and \
+                       unknown_technical_debt(args, 'nodeSelector'):
+                        raise("nodeSelector not found but required.")
+                    if 'namespace' in pod['metadata'] and kind != 'Pod' and \
+                       unknown_technical_debt(args, 'namespaceInTemplate'):
+                        raise("namespace found in inner template. its redundant.")
         on_each_template(func)
+        if WARNING['found'] and WERROR:
+            raise Exception('Found Warning when Werror set.')
