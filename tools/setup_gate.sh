1e35b9d31f08806f9c641442237ea70c2241798b
Kevin Fox <kevin@efox.cc>
2016-09-27 14:53:50 -0700

    WIP: Test setting up the gate
    TrivialFix
    
    Change-Id: I29d928b9864e268542a8a51d86b566e4933557bf

---
+++ tools/setup_gate.sh
@@ -0,0 +1,49 @@
+#!/bin/bash -xe
+
+echo Setting up the gate...
+env
+echo Setting up the gate...
+
+if [ -f /etc/redhat-release ];
+then
+	cat > /tmp/setup.$$ <<"EOF"
+cat <<"EOEF" > /etc/yum.repos.d/kubernetes.repo
+[kubernetes]
+name=Kubernetes
+baseurl=http://yum.kubernetes.io/repos/kubernetes-el7-x86_64
+enabled=1
+gpgcheck=1
+repo_gpgcheck=1
+gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg
+       https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
+EOEF
+yum install -y docker kubelet kubeadm kubectl kubernetes-cni
+systemctl start kubelet
+EOF
+else
+        cat > /tmp/setup.$$ <<"EOF"
+apt-get install -y apt-transport-https
+curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+echo "deb http://apt.kubernetes.io/ kubernetes-xenial main" > /etc/apt/sources.list.d/kubernetes.list
+apt-get update
+apt-get install -y docker.io kubelet kubeadm kubectl kubernetes-cni
+EOF
+fi
+
+sudo bash /tmp/setup.$$
+
+sudo systemctl start docker
+
+COUNT=0
+while true; do
+	docker ps -a && break || true
+        sleep 1
+        COUNT=$((COUNT+1))
+        [ $COUNT -gt 5 ] && break
+done
+
+sudo kubeadm init
+
+kubectl taint nodes --all dedicated-
+
+kubectl get pods
