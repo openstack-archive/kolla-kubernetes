5f797f4b422c5b1cef8720877970e719d8a0702e
Kevin Fox <kevin@efox.cc>
2016-09-27 14:53:50 -0700

    WIP: Test setting up the gate
    TrivialFix
    
    Change-Id: I29d928b9864e268542a8a51d86b566e4933557bf

--- tools/setup-resolv-conf.sh
+++ tools/setup-resolv-conf.sh
@@ -1,7 +1,6 @@
-#!/bin/bash
+#!/bin/bash -e
 NAMESPACE=$(kolla-kubernetes resource-template create bootstrap neutron-create-db -o json | jq -r '.metadata.namespace')
 TOOLBOX=$(kolla-kubernetes resource-template create bootstrap neutron-create-db -o json | jq -r '.spec.template.spec.containers[0].image')
 kubectl run -i --rm fetchresolv --restart=Never --namespace=$NAMESPACE --image=$TOOLBOX -- /bin/bash -c 'cat /etc/resolv.conf' | egrep '^search|^nameserver|^options' > /tmp/$$
 kubectl create configmap resolv-conf --from-file=resolv.conf=/tmp/$$ --namespace $NAMESPACE
 rm -f /tmp/$$
-
