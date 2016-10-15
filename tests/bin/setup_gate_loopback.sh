0ef5942b7702745f5b0a9f81b9ef564f4886631c
Kevin Fox <kevin@efox.cc>
2016-10-15 10:24:59 -0700

    Modular gate
    Start breaking up the gate into multiple scripts so that parts can
    be reused in the minikube environment and so that the gate will be
    more easily managed.
    
    Change-Id: Ic19cab0eb016d3dbc2df053c69347872b1f933bc

---
+++ tests/bin/setup_gate_loopback.sh
@@ -0,0 +1,20 @@
+#!/bin/bash -e
+cat > /tmp/setup.$$ <<"EOF"
+mkdir -p /data/kolla
+df -h
+dd if=/dev/zero of=/data/kolla/ceph-osd0.img bs=5M count=1024
+dd if=/dev/zero of=/data/kolla/ceph-osd1.img bs=5M count=1024
+LOOP=$(losetup -f)
+losetup $LOOP /data/kolla/ceph-osd0.img
+parted $LOOP mklabel gpt
+parted $LOOP mkpart 1 0% 512m
+parted $LOOP mkpart 2 513m 100%
+dd if=/dev/zero of=/data/kolla/ceph-osd1.img bs=5M count=1024
+LOOP=$(losetup -f)
+losetup $LOOP /data/kolla/ceph-osd1.img
+parted $LOOP mklabel gpt
+parted $LOOP mkpart 1 0% 512m
+parted $LOOP mkpart 2 513m 100%
+partprobe
+EOF
+sudo bash /tmp/setup.$$
