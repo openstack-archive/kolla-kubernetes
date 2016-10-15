1cb70436d45d8007197b3e33cccc639834ce21c8
Kevin Fox <kevin@efox.cc>
2016-10-15 10:24:59 -0700

    Modular gate
    Start breaking up the gate into multiple scripts so that parts can
    be reused in the minikube environment and so that the gate will be
    more easily managed.
    
    Change-Id: Ic19cab0eb016d3dbc2df053c69347872b1f933bc

---
+++ tests/bin/ceph_workflow.sh
@@ -0,0 +1,133 @@
+#!/bin/bash -xe
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/../.." && pwd )"
+
+kollakube res create configmap \
+    mariadb keystone horizon rabbitmq memcached nova-api nova-conductor \
+    nova-scheduler glance-api-haproxy glance-registry-haproxy glance-api \
+    glance-registry neutron-server neutron-dhcp-agent neutron-l3-agent \
+    neutron-metadata-agent neutron-openvswitch-agent openvswitch-db-server \
+    openvswitch-vswitchd nova-libvirt nova-compute nova-consoleauth \
+    nova-novncproxy nova-novncproxy-haproxy neutron-server-haproxy \
+    nova-api-haproxy cinder-api cinder-api-haproxy cinder-backup \
+    cinder-scheduler cinder-volume keepalived;
+
+kollakube res create secret nova-libvirt
+
+for x in mariadb rabbitmq; do
+    kollakube res create pv $x
+    kollakube res create pvc $x
+done
+
+kollakube res create svc mariadb memcached keystone-admin keystone-public \
+    rabbitmq rabbitmq-management nova-api glance-api glance-registry \
+    neutron-server nova-metadata nova-novncproxy horizon cinder-api
+
+kollakube res create bootstrap mariadb-bootstrap rabbitmq-bootstrap
+
+$DIR/tools/pull_containers.sh kolla
+$DIR/tools/wait_for_pods.sh kolla
+
+kollakube res delete bootstrap mariadb-bootstrap rabbitmq-bootstrap
+kollakube res create pod mariadb memcached rabbitmq
+
+$DIR/tools/wait_for_pods.sh kolla
+
+kollakube resource create bootstrap keystone-create-db keystone-endpoints \
+    keystone-manage-db
+
+$DIR/tools/pull_containers.sh kolla
+$DIR/tools/wait_for_pods.sh kolla
+
+kollakube resource delete bootstrap keystone-create-db keystone-endpoints \
+    keystone-manage-db
+
+kollakube res create pod keystone
+
+$DIR/tools/wait_for_pods.sh kolla
+
+kollakube res create bootstrap nova-create-keystone-user \
+    glance-create-keystone-user cinder-create-keystone-user \
+    neutron-create-keystone-user \
+    nova-create-keystone-endpoint-public \
+    glance-create-keystone-endpoint-public \
+    cinder-create-keystone-endpoint-public \
+    cinder-create-keystone-endpoint-publicv2 \
+    neutron-create-keystone-endpoint-public
+
+$DIR/tools/wait_for_pods.sh kolla
+
+kollakube res delete bootstrap nova-create-keystone-user \
+    glance-create-keystone-user cinder-create-keystone-user \
+    neutron-create-keystone-user \
+    nova-create-keystone-endpoint-public \
+    glance-create-keystone-endpoint-public \
+    cinder-create-keystone-endpoint-public \
+    cinder-create-keystone-endpoint-publicv2 \
+    neutron-create-keystone-endpoint-public
+
+kollakube res create bootstrap glance-create-db glance-manage-db \
+    nova-create-api-db nova-create-db neutron-create-db neutron-manage-db \
+    cinder-create-db cinder-manage-db \
+    nova-create-keystone-endpoint-internal \
+    glance-create-keystone-endpoint-internal \
+    cinder-create-keystone-endpoint-internal \
+    cinder-create-keystone-endpoint-internalv2 \
+    neutron-create-keystone-endpoint-internal \
+    nova-create-keystone-endpoint-admin \
+    glance-create-keystone-endpoint-admin \
+    cinder-create-keystone-endpoint-admin \
+    cinder-create-keystone-endpoint-adminv2 \
+    neutron-create-keystone-endpoint-admin
+
+$DIR/tools/pull_containers.sh kolla
+$DIR/tools/wait_for_pods.sh kolla
+
+[ -d "$WORKSPACE/logs" ] &&
+kubectl get jobs -o json > $WORKSPACE/logs/jobs-after-bootstrap.json \
+    --namespace=kolla || true
+
+$DIR/tools/build_local_admin_keystonerc.sh
+. ~/keystonerc_admin
+
+$DIR/tests/bin/endpoint_test.sh
+
+[ -d "$WORKSPACE/logs" ] && openstack catalog list > \
+    $WORKSPACE/logs/openstack-catalog-after-bootstrap.json || true
+
+kollakube res delete bootstrap glance-create-db glance-manage-db \
+    nova-create-api-db nova-create-db neutron-create-db neutron-manage-db \
+    cinder-create-db cinder-manage-db \
+    nova-create-keystone-endpoint-internal \
+    glance-create-keystone-endpoint-internal \
+    cinder-create-keystone-endpoint-internal \
+    cinder-create-keystone-endpoint-internalv2 \
+    neutron-create-keystone-endpoint-internal \
+    nova-create-keystone-endpoint-admin \
+    glance-create-keystone-endpoint-admin \
+    cinder-create-keystone-endpoint-admin \
+    cinder-create-keystone-endpoint-adminv2 \
+    neutron-create-keystone-endpoint-admin
+
+kollakube res create pod nova-api nova-conductor nova-scheduler glance-api \
+    glance-registry neutron-server horizon nova-consoleauth nova-novncproxy \
+    cinder-api cinder-scheduler cinder-volume-ceph openvswitch-ovsdb-network \
+    openvswitch-vswitchd-network
+
+$DIR/tools/pull_containers.sh kolla
+$DIR/tools/wait_for_pods.sh kolla
+
+kollakube res create pod neutron-dhcp-agent neutron-l3-agent-network \
+    neutron-openvswitch-agent-network neutron-metadata-agent-network
+
+kollakube res create bootstrap openvswitch-set-external-ip
+kollakube res create pod nova-libvirt
+kollakube res create pod nova-compute
+#kollakube res create pod keepalived
+
+$DIR/tools/pull_containers.sh kolla
+$DIR/tools/wait_for_pods.sh kolla
+
+kollakube res delete bootstrap openvswitch-set-external-ip
+
+$DIR/tools/wait_for_pods.sh kolla
