c48fc0044d74a5a623a57bda7eecc99d99c2dd33
Kevin Fox <kevin@efox.cc>
2016-10-20 10:57:37 -0700

    WIP: Test gate
    Change-Id: I9aaf4d1f85df78eca2295928ed1435de33ef1c4e

--- tests/bin/ceph_workflow.sh
+++ tests/bin/ceph_workflow.sh
@@ -29,7 +29,7 @@ $DIR/tools/pull_containers.sh kolla
 $DIR/tools/wait_for_pods.sh kolla
 
 kollakube res delete bootstrap mariadb-bootstrap rabbitmq-bootstrap
-kollakube res create pod mariadb memcached rabbitmq
+kollakube res create pod mariadb rabbitmq memcached
 
 $DIR/tools/wait_for_pods.sh kolla
 
