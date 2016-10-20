fd7bf6510e6ee0677ab7dd624afddeedb104175b
Kevin Fox <kevin@efox.cc>
2016-10-20 10:57:37 -0700

    WIP: Test gate
    Change-Id: I9aaf4d1f85df78eca2295928ed1435de33ef1c4e

--- tests/bin/endpoint_test.sh
+++ tests/bin/endpoint_test.sh
@@ -9,3 +9,8 @@ OS_TOKEN=$(openstack token issue -f value -c id)
 curl -H "X-Auth-Token:$OS_TOKEN" $OS_AUTH_URL/endpoints -o /tmp/$$
 jq -r '.endpoints[] | .service_id' /tmp/$$ | sort | uniq -c > /tmp/$$.1
 awk '{if($1 != 3){exit -1}}' /tmp/$$.1 || endpoints_dump_and_fail
+cp /tmp/$$ $WORKSPACE/logs/endpoints.txt
+cp /tmp/$$.1 $WORKSPACE/logs/endpoints1.txt
+openstack endpoint list > $WORKSPACE/logs/endpoints2.txt
+openstack service list > $WORKSPACE/logs/endpoints3.txt
+openstack catalog list > $WORKSPACE/logs/endpoints4.txt
