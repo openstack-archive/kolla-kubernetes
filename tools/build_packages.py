e1d6c1a98d70d9b435ac74a40a2098dd258327ce
Kevin Fox <kevin@efox.cc>
2016-11-10 07:38:07 -0800

    WIP helm
    Playing around with some ideas. Very very work in progress.
    
    Change-Id: I7e495ad04aec0832f3d145bae1596e294849a014

---
+++ tools/build_packages.py
@@ -0,0 +1,93 @@
+#!/usr/bin/python
+
+import shutil
+import subprocess
+import sys
+import os
+import tempfile
+
+def helm_build_package(repodir, srcdir):
+    command_line = "cd %s; helm package %s" %(repodir, srcdir)
+    try:
+        res = subprocess.check_output(
+            command_line, shell=True,
+            executable='/bin/bash')
+        res = res.strip()  # strip whitespace
+        print(res)
+    except subprocess.CalledProcessError as e:
+        print(e)
+        raise
+
+def build_packages(srcdir, repodir, tmpdir):
+    print "Building with %s %s %s" %(srcdir, repodir, tmpdir)
+    helm_build_package(repodir, os.path.join(srcdir, "openstack-kolla-common"))
+
+    packages = [{
+        'name': 'neutron',
+        'subpackages': [{
+            'name':'l3-agent',
+            'type':'daemonset'
+        }]
+    }]
+
+    for package in packages:
+        for subpackage in package['subpackages']:
+            packagedir = os.path.join(tmpdir, "%s-%s" %(package['name'], subpackage['name']))
+            print packagedir
+            os.mkdir(packagedir)
+            packagetemplatedir = os.path.join(packagedir, "templates")
+            os.mkdir(packagetemplatedir)
+            os.mkdir(os.path.join(packagedir, "charts"))
+
+            #FIXME unhardcode this
+            shutil.copy(os.path.join(repodir, 'openstack-kolla-common-2.0.2-1.tgz'),
+                        os.path.join(packagedir, 'charts', 'openstack-kolla-common-2.0.2-1.tgz'))
+
+            template = "%s_%s.yaml" %(subpackage['name'].replace('-', '_'), subpackage['type'])
+            shutil.copy(os.path.join(srcdir, 'src', 'common_main.yaml'),
+                        os.path.join(packagetemplatedir, 'main.yaml'))
+            shutil.copy(os.path.join(srcdir, 'src', package['name'], 'templates', template),
+                        os.path.join(packagetemplatedir, "_main.yaml"))
+            shutil.copy(os.path.join(srcdir, 'src', package['name'], 'values', template),
+                        os.path.join(packagedir, 'values.yaml'))
+
+            f = open(os.path.join(srcdir, 'src', 'common_values.yaml'), 'r')
+            lines = f.read()
+            f.close()
+            f = open(os.path.join(packagedir, 'values.yaml'), 'a')
+            f.write(lines)
+            f.close()
+
+            f = open(os.path.join(srcdir, 'src', 'common_chart.yaml'), 'r')
+            lines = f.read()
+            f.close()
+            f = open(os.path.join(packagedir, 'Chart.yaml'), 'a')
+            lines = lines.replace('@NAME@', "%s-%s" %(package['name'], subpackage['name']))
+            f.write(lines)
+            f.close()
+
+#cat $DIR/src/neutron/${undername}_daemonset.yaml >> $TMPDIR/$package/values.yaml
+#cat $DIR/src/common_values.yaml >> $TMPDIR/$package/values.yaml
+#helm package $TMPDIR/$package
+            helm_build_package(repodir, packagedir)
+
+def main():
+    path = os.path.abspath(os.path.dirname(sys.argv[0]))
+    srcdir = os.path.join(path, "../helm")
+    if len(sys.argv) < 2:
+        sys.stderr.write("You must specify the repo directory to build in.\n")
+        sys.exit(-1)
+    repodir = sys.argv[1]
+    if not os.path.isdir(repodir):
+        sys.stderr.write("The specified repo directory does not exist.\n")
+        sys.exit(-1)
+    tmpdir = tempfile.mkdtemp()
+    try:
+        build_packages(srcdir, repodir, tmpdir)
+    except:
+        shutil.rmtree(tmpdir)
+        raise
+    shutil.rmtree(tmpdir)
+
+if __name__ == '__main__':
+    sys.exit(main())
