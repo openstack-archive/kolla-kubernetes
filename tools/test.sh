453ff9d72beca3e6fda4f2236924d8c7f0fc6a38
Kevin Fox <kevin@efox.cc>
2016-11-11 05:08:14 -0800

    Unbreak gate from recent kolla change
    TrivialFix
    
    Change-Id: I8a374ed613ff2b80f154913bcd35d8b4ef3f2840

--- tools/test.sh
+++ tools/test.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 [ ! -d ../kolla ] && pushd .. && git clone https://github.com/openstack/kolla && popd
 grep api_interface_address ../kolla/etc/kolla/globals.yml || echo api_interface_address: "0.0.0.0" >> ../kolla/etc/kolla/globals.yml
+grep tunnel_interface_address ../kolla/etc/kolla/globals.yml || echo tunnel_interface_address: "0.0.0.0" >> ../kolla/etc/kolla/globals.yml
 grep orchestration_engine ../kolla/etc/kolla/globals.yml || echo orchestration_engine: KUBERNETES >> ../kolla/etc/kolla/globals.yml
 python setup.py test --slowest --testr-args="$1"
