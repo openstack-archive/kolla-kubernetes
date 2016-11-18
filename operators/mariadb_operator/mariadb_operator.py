eb731d14e037bf499f654f0aedd2404e15a3a272
Ryan Hallisey <rhallise@redhat.com>
2016-11-17 19:29:55 -0500

    Mariadb Kubernetes Operator
    Change-Id: I36338dd68a56a6cbf57a821eb4c3d89f323776a9
    Partially-implements: blueprint mariadb-operator

---
+++ operators/mariadb_operator/mariadb_operator.py
@@ -0,0 +1,39 @@
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Mariadb Operator class."""
+
+
+from service_operator import ServiceOperator
+
+
+class MariadbOperator(ServiceOperator):
+
+    def __init__(self, data):
+        self.data = data
+        self.template = self.data.get('kollaSpec')
+        self.dependencies = self.template.get('dependencies')
+        self.name = self.template.get('serviceName')
+
+        self.lifecycle_operations = ['deploy, recovery']
+        self.operation = self.template.get('action')
+        super(MariadbOperator, self).__init__(data)
+
+    def _mariadb_recovery(self):
+        """Workflow for recovering mariadb"""
+
+        pass
+
+    def deploy(self):
+        self._database_action()
+        self._keystone_action()
+        self._run_helm_package()
