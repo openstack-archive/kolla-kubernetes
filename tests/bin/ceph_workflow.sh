1f937b9872d44fe4d8b740e150b894b0c3c0fbc4
Justin Scott <justin.a.scott@intel.com>
2016-12-01 23:11:06 +0000

    WIP Helm-ize memcached
    Work in progress. This PS includes templates for memcached
    
    Change-Id: Ie82559744cc0114a79fe845c940b81193298102b

--- tests/bin/ceph_workflow.sh
+++ tests/bin/ceph_workflow.sh
@@ -37,7 +37,7 @@ $DIR/tools/pull_containers.sh kolla
 $DIR/tools/wait_for_pods.sh kolla
 
 kollakube res delete bootstrap mariadb-bootstrap rabbitmq-bootstrap
-kollakube res create pod mariadb memcached rabbitmq
+kollakube res create pod mariadb rabbitmq
 
 $DIR/tools/wait_for_pods.sh kolla
 
@@ -126,6 +126,10 @@ helm ls
 helm install kolla/neutron-server --version 3.0.0-1 \
     --set enable_kube_logger=false \
     --namespace kolla --name neutron-server
+
+helm install kolla/memcached --version 3.0.0-1 \
+    --set enable_kube_logger=false \
+    --namespace kolla --name memcached
 
 $DIR/tools/pull_containers.sh kolla
 $DIR/tools/wait_for_pods.sh kolla
