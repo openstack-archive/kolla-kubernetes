e554860ba330c01d7f401c2983bf59a2887d37d1
Justin Scott <justin.a.scott@intel.com>
2016-12-01 23:11:06 +0000

    WIP Helm-ize memcached
    Work in progress. This PS includes templates for memcached
    
    Change-Id: Ie82559744cc0114a79fe845c940b81193298102b

--- tests/bin/ceph_workflow.sh
+++ tests/bin/ceph_workflow.sh
@@ -66,9 +66,14 @@ $DIR/tools/wait_for_pods.sh kolla
 
 kollakube res delete bootstrap mariadb-bootstrap
 
+kollakube res create pod mariadb
+
 helm delete rabbitmq-job
 
-kollakube res create pod mariadb memcached
+helm install kolla/memcached --version 3.0.0-1 \
+    --set "enable_kube_logger=false,element_name=memcached" \
+    --namespace kolla --name memcached
+
 
 helm install kolla/rabbitmq-pod --debug --version 3.0.0-1 \
     --namespace kolla --name rabbitmq-pod --set "$common_vars,element_name=rabbitmq"
