f9ad069963a36f2b7feb2247a3a083e1253435d0
portdirect <pete@port.direct>
2016-12-02 01:56:28 +0000

    WIP: Add Development Setup Script for use with Kubernetes Helm
    This ps is just so I can move my work home - please review
    the previous ps.
    
    This brings in a helper script for setting up a development
    environment for building and deploying Kolla-Kubernetes locally.
    
    Change-Id: I87e7a722ae611154311f330106023b3adfb629d0

---
+++ tests/bin/setup_dev_config.sh
@@ -0,0 +1,48 @@
+#!/bin/bash -xe
+
+NODE=$(hostname -s)
+
+TYPE="$2"
+
+echo "kolla_base_distro: $1" >> kolla/etc/kolla/globals.yml
+cat tests/conf/ceph-all-in-one/kolla_config >> kolla/etc/kolla/globals.yml
+IP=172.18.0.1
+sed -i "s/^\(kolla_external_vip_address:\).*/\1 '$IP'/" \
+    kolla/etc/kolla/globals.yml
+sed -i "s/^\(kolla_kubernetes_external_vip:\).*/\1 '$IP'/" \
+    etc/kolla-kubernetes/kolla-kubernetes.yml
+
+cat tests/conf/ceph-all-in-one/kolla_kubernetes_config \
+    >> etc/kolla-kubernetes/kolla-kubernetes.yml
+
+sed -i "s/initial_mon:.*/initial_mon: $NODE/" \
+    etc/kolla-kubernetes/kolla-kubernetes.yml
+
+if [ "x$TYPE" == "xceph-multi" ]; then
+    interface="eth1"
+    echo "tunnel_interface: $interface" >> kolla/etc/kolla/globals.yml
+    echo "storage_interface: $interface" >> \
+        etc/kolla-kubernetes/kolla-kubernetes.yml
+    sed -i "s/172.17.0.1/$(cat /etc/nodepool/primary_node_private)/" \
+        etc/kolla-kubernetes/kolla-kubernetes.yml
+fi
+
+kolla/tools/generate_passwords.py
+kolla/tools/kolla-ansible genconfig
+
+crudini --set /etc/kolla/nova-compute/nova.conf libvirt virt_type qemu
+crudini --set /etc/kolla/nova-compute/nova.conf libvirt rbd_user nova
+UUID=$(awk '{if($1 == "rbd_secret_uuid:"){print $2}}' /etc/kolla/passwords.yml)
+crudini --set /etc/kolla/nova-compute/nova.conf libvirt rbd_secret_uuid $UUID
+
+# Keystone does not seem to invalidate its cache on entry point addition.
+crudini --set /etc/kolla/keystone/keystone.conf cache enabled False
+
+sed -i 's/log_outputs = "3:/log_outputs = "1:/' /etc/kolla/nova-libvirt/libvirtd.conf
+sed -i 's/log_level = 3/log_level = 1/' /etc/kolla/nova-libvirt/libvirtd.conf
+
+sed -i \
+    '/\[global\]/a osd pool default size = 1\nosd pool default min size = 1\nosd crush chooseleaf type = 0\ndebug default = 5\n'\
+    /etc/kolla/ceph*/ceph.conf
+
+./tools/fix-mitaka-config.py
