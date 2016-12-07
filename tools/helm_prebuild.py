f87f4bc5d0eb4fd9d0ca34750bac9bfa74ffe380
portdirect <pete@port.direct>
2016-12-07 15:08:57 +0000

    Change _common_deployment to _common_api_python_deployment
    Rename _common_deployment.yaml to _common_api_python_deployment.yaml,
    to make the scope of this template clearer. Also makes haproxy optional.
    
    Change-Id: Iec52782a5226aa5a9d1d904edc2bf9f24e4af4d7

--- tools/helm_prebuild.py
+++ tools/helm_prebuild.py
@@ -23,6 +23,9 @@ stateful_services = [
     'rabbitmq-pv'
 ]
 
+pod_http_termination = [
+    'neutron-server'
+]
 
 def helm_build_package(repodir, srcdir):
     command_line = "cd %s; helm package %s" % (repodir, srcdir)
@@ -58,6 +61,8 @@ def main():
                 raise
         helm_build_package(pkgchartdir, os.path.join(srcdir, "kolla-common"))
         pkg_values = copy.deepcopy(values['common'])
+        if package in pod_http_termination:
+            pkg_values.update(values['pod-http-termination'])
         if package in stateful_services:
             pkg_values.update(values['stateful-service'])
         try:
