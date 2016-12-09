0a62fafd70918e0aed661830f203e6e9dbce6613
Kevin Fox <kevin@efox.cc>
2016-12-08 18:35:12 -0800

    Have gate wait for dns ready
    Kubernetes does not seem to properly set the Ready state on dns being
    usable. This seems to be causing intermittent failures in the gate.
    This ps adds a test to ensure dns is responsive on all nodes before
    continuing.
    
    Change-Id: I9e871c041dbdda6dac74c1cb283787550887237c

---
+++ tools/test_kube_dns.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/" && pwd )"
+
+TOOLBOX=$(kolla-kubernetes resource-template create bootstrap neutron-create-db -o json | jq -r '.spec.template.spec.containers[0].image')
+
+kubectl get nodes -o json | jq -r '.items[].metadata.name' | while read NODE; do
+    RELEASE="test-dns-$NODE"
+    kubectl create -f <(cat <<EOF
+apiVersion: batch/v1
+kind: Job
+metadata:
+  name: $RELEASE
+  namespace: default
+spec:
+  template:
+    spec:
+      nodeSelector:
+          kubernetes.io/hostname: $NODE
+      containers:
+        - image: "$TOOLBOX"
+          name: main
+          command: ["sh", "-xec"]
+          args:
+            - python -c 'import socket; print socket.gethostbyname("google.com"), socket.gethostbyname("kubernetes.default")'
+      restartPolicy: OnFailure
+EOF
+)
+done
+
+$DIR/wait_for_pods.sh default
+
+kubectl get nodes -o json | jq -r '.items[].metadata.name' | while read NODE; do
+    RELEASE="test-dns-$NODE"
+    kubectl delete job $RELEASE
+done
+
