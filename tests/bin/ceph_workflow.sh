cfe84c600f3eff6a4cfae578e78a19f0b31e7220
Mark Giles <mark.giles@oracle.com>
2016-12-10 16:54:13 -0500

    Helm database creation for Nova
    Change-Id: I701a579e35ac1fc87bd5e8f40c4863feedd37f43
    Partially-Implements: blueprint helm-microservices

--- tests/bin/ceph_workflow.sh
+++ tests/bin/ceph_workflow.sh
@@ -133,7 +133,7 @@ kollakube res delete bootstrap nova-create-keystone-user \
     neutron-create-keystone-endpoint-public
 
 kollakube res create bootstrap glance-create-db glance-manage-db \
-    nova-create-api-db nova-create-db neutron-create-db neutron-manage-db \
+    neutron-create-db neutron-manage-db \
     cinder-create-db cinder-manage-db \
     nova-create-keystone-endpoint-internal \
     glance-create-keystone-endpoint-internal \
@@ -146,8 +146,27 @@ kollakube res create bootstrap glance-create-db glance-manage-db \
     cinder-create-keystone-endpoint-adminv2 \
     neutron-create-keystone-endpoint-admin
 
+for x in nova nova-api; do
+    helm install kolla/$x-create-db --version 3.0.0-1 \
+        --set element_name=$x --namespace kolla \
+        --name $x-create-db
+done
+
 $DIR/tools/pull_containers.sh kolla
 $DIR/tools/wait_for_pods.sh kolla
+
+for x in nova nova-api; do
+    helm delete --purge $x-create-db
+done
+
+helm install kolla/nova-api-manage-db --version 3.0.0-1 \
+    --set element_name=nova-api --namespace kolla \
+    --name nova-api-manage-db
+
+$DIR/tools/pull_containers.sh kolla
+$DIR/tools/wait_for_pods.sh kolla
+
+helm delete --purge nova-api-manage-db
 
 [ -d "$WORKSPACE/logs" ] &&
 kubectl get jobs -o json > $WORKSPACE/logs/jobs-after-bootstrap.json \
