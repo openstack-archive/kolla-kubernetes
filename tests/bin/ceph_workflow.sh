011a39d97e7937a4043be3e9d2678d4452ba0b76
Kevin Fox <kevin@efox.cc>
2016-12-13 09:31:51 -0800

    Helmize neutron db stuff and add svc.
    This ps adds neutron bits for setting up the database and adds the
    service.
    
    Change-Id: I8c119025c6a4ea56d998a6b2f266888c66c0d56b
    Partially-Implements: blueprint helm-microservices

--- tests/bin/ceph_workflow.sh
+++ tests/bin/ceph_workflow.sh
@@ -54,7 +54,9 @@ helm install kolla/glance-api-svc --version 3.0.0-1 \
 helm install kolla/glance-registry-svc --version 3.0.0-1 \
     --namespace kolla --name glance-registry-svc
 
-kollakube res create svc neutron-server
+helm install kolla/neutron-server-svc --version 3.0.0-1 \
+    --namespace kolla --name neutron-server-svc \
+    --set "element_port_external=true,kolla_kubernetes_external_vip=$IP"
 
 helm install kolla/cinder-api-svc --version 3.0.0-1 \
     --namespace kolla --name cinder-api-svc \
@@ -201,7 +203,6 @@ helm delete --purge neutron-create-keystone-service
 helm delete --purge neutron-create-keystone-endpoint-public
 
 kollakube res create bootstrap glance-create-db glance-manage-db \
-    neutron-create-db neutron-manage-db \
 
 helm install kolla/cinder-create-db --version 3.0.0-1 \
     --set element_name=cinder \
@@ -222,7 +223,7 @@ kollakube res create bootstrap nova-create-keystone-endpoint-internal \
     cinder-create-keystone-endpoint-admin \
     cinder-create-keystone-endpoint-adminv2
 
-for x in nova nova-api; do
+for x in nova nova-api neutron; do
     helm install kolla/$x-create-db --version 3.0.0-1 \
         --set element_name=$x --namespace kolla \
         --name $x-create-db
@@ -231,18 +232,14 @@ done
 $DIR/tools/pull_containers.sh kolla
 $DIR/tools/wait_for_pods.sh kolla
 
-for x in nova nova-api; do
-    helm delete --purge $x-create-db
+for x in nova-api neutron; do
+    helm install kolla/$x-manage-db --version 3.0.0-1 \
+        --set element_name=$x --namespace kolla \
+        --name $x-manage-db
 done
-
-helm install kolla/nova-api-manage-db --version 3.0.0-1 \
-    --set element_name=nova-api --namespace kolla \
-    --name nova-api-manage-db
 
 $DIR/tools/pull_containers.sh kolla
 $DIR/tools/wait_for_pods.sh kolla
-
-helm delete --purge nova-api-manage-db
 
 [ -d "$WORKSPACE/logs" ] &&
 kubectl get jobs -o json > $WORKSPACE/logs/jobs-after-bootstrap.json \
@@ -253,12 +250,15 @@ $DIR/tests/bin/endpoint_test.sh
 [ -d "$WORKSPACE/logs" ] && openstack catalog list > \
     $WORKSPACE/logs/openstack-catalog-after-bootstrap.json || true
 
-kollakube res delete bootstrap glance-create-db glance-manage-db \
-    neutron-create-db neutron-manage-db
+kollakube res delete bootstrap glance-create-db glance-manage-db
 
-helm delete --purge cinder-create-db
+for x in nova nova-api cinder neutron; do
+    helm delete --purge $x-create-db
+done
 
-helm delete --purge cinder-manage-db
+for x in nova-api cinder neutron; do
+    helm delete --purge $x-manage-db
+done
 
 kollakube res delete bootstrap \
     nova-create-keystone-endpoint-internal \
