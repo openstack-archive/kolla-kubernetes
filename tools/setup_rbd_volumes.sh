3e070e6b22fbb0519752ee95536b6d11e229a7c4
Serguei Bezverkhi <sbezverk@cisco.com>
2016-12-13 15:48:03 -0500

    Glance service orchestration with entrypoint
    This PS adds Glance service orchestration with entrypoint.
    
    Change-Id: Iba26ec16a7fb14f240777001deff1a567715de0a
    Partially-Implements: blueprint helm-services

--- tools/setup_rbd_volumes.sh
+++ tools/setup_rbd_volumes.sh
@@ -13,8 +13,10 @@ str="timeout 60s rbd create kollavolumes/rabbitmq --size 1024"
 kubectl exec ceph-admin -c main --namespace=kolla -- /bin/bash -c "$str"
 str="timeout 60s rbd create kollavolumes/helm-repo --size 1024"
 kubectl exec ceph-admin -c main --namespace=kolla -- /bin/bash -c "$str"
+str="timeout 60s rbd create kollavolumes/glance --size 1024"
 
-for volume in mariadb rabbitmq helm-repo; do
+kubectl exec ceph-admin -c main --namespace=kolla -- /bin/bash -c "$str"
+for volume in mariadb rabbitmq glance helm-repo; do
     str='DEV=$(rbd map --pool kollavolumes '$volume'); mkfs.xfs $DEV;'
     str="$str rbd unmap "'$DEV;'
     timeout 60s kubectl exec ceph-admin -c main --namespace=kolla -- \
