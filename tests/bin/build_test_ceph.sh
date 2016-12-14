4e22b97466f569c15ceb905f600858e9487a9030
portdirect <pete@port.direct>
2016-12-14 09:40:09 +0000

    Helm Ceph Initial Mon
    Co-Authored-By: Duong Ha-Quang <duonghq@vn.fujitsu.com>
    
    Change-Id: I6bc9b38b2c6bfde625265bc5cc5c88c084c9afcd
    Partially-Implements: blueprint helm-microservices

--- tests/bin/build_test_ceph.sh
+++ tests/bin/build_test_ceph.sh
@@ -1,6 +1,25 @@
 #!/bin/bash -xe
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/../.." && pwd )"
+IP="$3"
+base_distro="$2"
+gate_job="$1"
+tunnel_interface="$4"
+
+. "$DIR/tests/bin/common_workflow_config.sh"
+
+function general_config {
+    common_workflow_config $IP $base_distro $tunnel_interface
+}
+
+function ceph_config {
+  echo "node: $(hostname -s)"
+  echo "storage_interface: $tunnel_interface"
+  echo "initial_member: $(hostname -s)"
+}
+
+general_config > /tmp/general_config.yaml
+ceph_config > /tmp/ceph_config.yaml
 
 function wait_for_ceph_bootstrap {
     set +x
@@ -19,13 +38,16 @@ function wait_for_ceph_bootstrap {
 
 kollakube res create configmap ceph-mon ceph-osd
 
-kollakube res create bootstrap ceph-bootstrap-initial-mon
+helm install kolla/test-ceph-init-mon-job --version 0.5.0-1 \
+    --namespace kolla \
+    --name test-ceph-init-mon-job \
+    --values /tmp/general_config.yaml \
+    --values /tmp/ceph_config.yaml
 
 $DIR/tools/pull_containers.sh kolla
 $DIR/tools/wait_for_pods.sh kolla
 
 $DIR/tools/setup-ceph-secrets.sh
-kollakube res delete bootstrap ceph-bootstrap-initial-mon
 kollakube res create pod ceph-mon
 
 $DIR/tools/wait_for_pods.sh kolla
