802867e5c249715c4f661cc9761f38036fa41c36
Kevin Fox <kevin@efox.cc>
2016-12-17 14:16:16 -0800

    WIP: neutron service package
    Change-Id: I4798acce844faa80f7c9e472465a36528a700bd4

---
+++ tests/bin/setup_helm_entrypint_config.sh
@@ -0,0 +1,27 @@
+function helm_entrypoint_neutron {
+    for x in neutron-create-keystone-service neutron-create-keystone-user \
+            neutron-create-keystone-endpoint-internal \
+            neutron-create-keystone-endpoint-admin neutron-create-db \
+            neutron-manage-db neutron-server neutron-metadata-agent; \
+        do
+            echo "$x:"
+            echo "    enable_kube_logger: false"
+            echo "    kolla_base_distro: $base_distro"
+    done
+    for x in neutron-dhcp-agent neutron-l3-agent neutron-openvswitch-agent; \
+        do
+            echo "$x:"
+            echo "    enable_kube_logger: false"
+            echo "    kolla_base_distro: $base_distro"
+            echo "    tunnel_interface: $tunnel_interface"
+    done
+    echo "neutron-create-keystone-endpoint-public:"
+    echo "    enable_kube_logger: false"
+    echo "    kolla_base_distro: $base_distro"
+    echo "    kolla_kubernetes_external_vip: 172.18.0.1"
+    echo "neutron-server-svc:"
+    echo "    enable_kube_logger: false"
+    echo "    kolla_base_distro: $base_distro"
+    echo "    element_port_external: true"
+    echo "    kolla_kubernetes_external_vip: 172.18.0.1"
+}
