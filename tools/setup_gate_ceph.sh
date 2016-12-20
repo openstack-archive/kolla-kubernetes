7a0ab052dfc53e872ed759f2a62493311eee20ce
Kevin Fox <kevin@efox.cc>
2016-12-20 09:49:16 -0800

    WIP: test rebooting in the gate
    Change-Id: I6acd40d41ff9b4d6c3f9ca7fae6ecdde8844cb05

--- tools/setup_gate_ceph.sh
+++ tools/setup_gate_ceph.sh
@@ -152,5 +152,7 @@ fi
 kubectl get pods --namespace=kolla
 kubectl get svc --namespace=kolla
 tests/bin/basic_tests.sh
-tests/bin/cleanup_tests.sh
+if [ "x$REBOOT_TEST" == "x" ]; then
+    tests/bin/cleanup_tests.sh
+fi
 tests/bin/build_docker_images.sh $WORKSPACE/logs $DISTRO $TYPE $CONFIG $BRANCH $PIPELINE
