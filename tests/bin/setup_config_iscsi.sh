d72468999d7e7c343d5d175a09f88971ea60ab97
Serguei Bezverkhi <sbezverk@cisco.com>
2016-12-24 12:09:59 -0500

    WIP Adding iscsi support to the gate.
    Currently gate utilizes CEPH as Cinder backend. This PS adds
    iscsi/lvm cinder backend support. NOTE: kubernetes cluster uses
    "host" storage proivder for any persistent volumes.
    
    Change-Id: Id0a9958b1b621aff0452b236e9e9180af8177aba

---
+++ tests/bin/setup_config_iscsi.sh
@@ -0,0 +1,29 @@
+#!/bin/bash -xe
+
+NODE=$(hostname -s)
+
+TYPE="$2"
+
+echo "kolla_base_distro: $1" >> kolla-ansible/etc/kolla/globals.yml
+cat tests/conf/iscsi-all-in-one/kolla_config >> kolla-ansible/etc/kolla/globals.yml
+IP=172.18.0.1
+sed -i "s/^\(kolla_external_vip_address:\).*/\1 '$IP'/" \
+    kolla-ansible/etc/kolla/globals.yml
+sed -i "s/^\(kolla_kubernetes_external_vip:\).*/\1 '$IP'/" \
+    etc/kolla-kubernetes/kolla-kubernetes.yml
+
+cat tests/conf/iscsi-all-in-one/kolla_kubernetes_config \
+    >> etc/kolla-kubernetes/kolla-kubernetes.yml
+
+kolla-ansible/tools/generate_passwords.py
+kolla-ansible/tools/kolla-ansible genconfig
+
+crudini --set /etc/kolla/nova-compute/nova.conf libvirt virt_type qemu
+
+# Keystone does not seem to invalidate its cache on entry point addition.
+crudini --set /etc/kolla/keystone/keystone.conf cache enabled False
+
+sed -i 's/log_outputs = "3:/log_outputs = "1:/' /etc/kolla/nova-libvirt/libvirtd.conf
+sed -i 's/log_level = 3/log_level = 1/' /etc/kolla/nova-libvirt/libvirtd.conf
+
+./tools/fix-mitaka-config.py
