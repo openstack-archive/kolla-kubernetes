ea8202e642842ac5b51ccdad91ff4a4dc53679d1
dupengfei <58750307@qq.com>
2016-12-26 12:14:22 +0800

    Helm charts for heat/heat-cfn create user and endpoints.
    Add heat-create-keystone-endpoint-admin
        heat-create-keystone-endpoint-internal
        heat-create-keystone-endpoint-public
        heat-create-keystone-service
        heat-cfn-create-keystone-endpoint-admin
        heat-cfn-create-keystone-endpoint-internal
        heat-cfn-create-keystone-endpoint-public
        heat-cfn-create-keystone-service
        heat-create-keystone-user
    Partially implements the heat.
    
    Change-Id: I59d48ac37dd657394c92b3578505dfdd149ae21e
    Partially-Implements: blueprint helm-microservices

--- tests/bin/ceph_workflow.sh
+++ tests/bin/ceph_workflow.sh
@@ -224,12 +224,27 @@ helm install kolla/neutron-create-keystone-endpoint-internal --version 3.0.0-1 \
 helm install kolla/neutron-create-keystone-endpoint-admin --version 3.0.0-1 \
     --namespace kolla --name neutron-create-keystone-endpoint-admin --set "$common_vars"
 
+helm install kolla/heat-create-keystone-user --debug --version 3.0.0-1 \
+    --namespace kolla --name heat-create-keystone-user
+
+for x in heat heat-cfn; do
+    helm install kolla/$x-create-keystone-service --version 3.0.0-1 \
+        --namespace kolla --name $x-create-keystone-service --set "$common_vars"
+    helm install kolla/$x-create-keystone-endpoint-public --version 3.0.0-1 \
+        --namespace kolla --name $x-create-keystone-endpoint-public --set "$common_vars,kolla_kubernetes_external_vip=172.18.0.1"
+    helm install kolla/$x-create-keystone-endpoint-internal --version 3.0.0-1 \
+        --namespace kolla --name $x-create-keystone-endpoint-internal --set "$common_vars"
+    helm install kolla/$x-create-keystone-endpoint-admin --version 3.0.0-1 \
+        --namespace kolla --name $x-create-keystone-endpoint-admin --set "$common_vars"
+done
+
+
 $DIR/tools/wait_for_pods.sh kolla
 
 kollakube res delete bootstrap \
     cinder-create-keystone-endpoint-publicv2
 
-for x in cinder glance neutron nova; do
+for x in cinder glance neutron nova heat; do
     helm delete --purge $x-create-keystone-user
 done
 
@@ -310,7 +325,7 @@ kollakube res delete bootstrap \
     cinder-create-keystone-endpoint-internalv2 \
     cinder-create-keystone-endpoint-adminv2
 
-for x in glance neutron cinder nova; do
+for x in glance neutron cinder nova heat heat-cfn; do
     helm delete --purge $x-create-keystone-service
     helm delete --purge $x-create-keystone-endpoint-public
     helm delete --purge $x-create-keystone-endpoint-internal
