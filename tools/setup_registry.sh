805bed2db46ab957f7cb96950f439f8e478c9270
Michal (inc0) Jastrzebski <inc007@gmail.com>
2017-01-10 19:32:35 +0000

    Local registry in gate for trunk builds
    This PS adds trunk support to the gate, as well as local repo
    caching for trunk.
    
    Co-Authored-By: Kevin Fox <Kevin.Fox@pnnl.gov>
    
    Change-Id: I6d33b7caa0faff37df3696196fdf588f7c633a6f

---
+++ tools/setup_registry.sh
@@ -0,0 +1,25 @@
+function registry_file {
+    BASE_DISTRO=$1
+    INSTALL_TYPE=$2
+    filename=${BASE_DISTRO}-${INSTALL_TYPE}-registry-${ZUUL_BRANCH}.tar.gz
+    echo $filename
+}
+
+function setup_registry {
+    BASE_DISTRO=$1
+    INSTALL_TYPE=$2
+    KEEP_TAR=$3
+    TAR_DIR=$4
+    filename=$(registry_file $BASE_DISTRO $INSTALL_TYPE)
+    if [ ! -f $TAR_DIR/$filename ]; then
+        echo Downloading $filename
+        wget -q -c -O $TAR_DIR/$filename \
+            http://tarballs.openstack.org/kolla/images/$filename
+    fi
+    sudo mkdir /tmp/kolla_registry
+    sudo chmod -R 644 /tmp/kolla_registry
+    sudo tar xzf $TAR_DIR/$filename -C /tmp/kolla_registry
+    [ "xKEEP_TAR" != "x" ] && rm -f $TAR_DIR/$filename
+    sudo chmod -R +x /tmp/kolla_registry
+    sudo docker run -d -p 4000:5000 --restart=always -v /tmp/kolla_registry/:/var/lib/registry --name registry registry:2
+}
