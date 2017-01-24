3033a7ff2690ba21574f7c60bcb1d468ea71f181
Serguei Bezverkhi <sbezverk@cisco.com>
2017-01-24 10:19:13 -0500

    WIP Add cinder control package with kubernetes entrypoint
    PS adds cinder control package deployment with kubernetes entrypoint.
    Control package includes cinder api and scheduler. Backup microservice
    is not included.
    
    Change-Id: I9f0f4c294aaa727b025f4eb1f74a1b1690c6e541

--- tests/bin/ceph_workflow_service.sh
+++ tests/bin/ceph_workflow_service.sh
@@ -80,10 +80,6 @@ helm install kolla/rabbitmq --version $VERSION \
     --namespace kolla --name rabbitmq --set "$common_vars" \
     --values <(helm_entrypoint_general $1)
 
-helm install kolla/cinder-api-svc --version $VERSION \
-    --namespace kolla --name cinder-api-svc \
-    --set "element_name=cinder,port_external=true,external_vip=$IP"
-
 helm install kolla/nova-api-svc --version $VERSION \
     --namespace kolla --name nova-api-svc \
     --set "element_name=nova,port_external=true,external_vip=$IP"
@@ -107,59 +103,24 @@ $DIR/tools/wait_for_pods.sh kolla
 $DIR/tools/build_local_admin_keystonerc.sh
 . ~/keystonerc_admin
 
-helm install kolla/cinder-create-keystone-service-job --version $VERSION \
-    --namespace kolla --name cinder-create-keystone-service --set "$common_vars"
-
-helm install kolla/cinder-create-keystone-servicev2-job --version $VERSION \
-    --namespace kolla --name cinder-create-keystone-servicev2 --set "$common_vars"
-
-helm install kolla/cinder-create-keystone-user-job --version $VERSION \
-    --namespace kolla --name cinder-create-keystone-user --set "$common_vars"
-
 helm install kolla/nova-create-keystone-user-job --version $VERSION \
     --namespace kolla --name nova-create-keystone-user --set "$common_vars"
 
 kollakube res create bootstrap \
     nova-create-keystone-endpoint-public
 
-helm install kolla/cinder-create-keystone-endpoint-public-job --version $VERSION \
-    --namespace kolla --name cinder-create-keystone-endpoint-public --set "$common_vars,external_vip=172.18.0.1"
-helm install kolla/cinder-create-keystone-endpoint-publicv2-job --version $VERSION \
-    --namespace kolla --name cinder-create-keystone-endpoint-publicv2 --set "$common_vars,external_vip=172.18.0.1"
-
 $DIR/tools/wait_for_pods.sh kolla
 
 kollakube res delete bootstrap \
     nova-create-keystone-endpoint-public
 
-for x in cinder nova; do
+for x in nova; do
     helm delete --purge $x-create-keystone-user
 done
 
-helm install kolla/cinder-create-db-job --version $VERSION \
-    --set $common_vars,element_name=cinder \
-    --namespace kolla \
-    --name cinder-create-db
-
-helm install kolla/cinder-manage-db-job --version $VERSION \
-    --set $common_vars,element_name=cinder \
-    --namespace kolla \
-    --name cinder-manage-db
 
 kollakube res create bootstrap nova-create-keystone-endpoint-internal \
     nova-create-keystone-endpoint-admin
-
-helm install kolla/cinder-create-keystone-endpoint-internal-job --version $VERSION \
-    --namespace kolla --name cinder-create-keystone-endpoint-internal --set "$common_vars"
-
-helm install kolla/cinder-create-keystone-endpoint-internalv2-job --version $VERSION \
-    --namespace kolla --name cinder-create-keystone-endpoint-internalv2 --set "$common_vars"
-
-helm install kolla/cinder-create-keystone-endpoint-admin-job --version $VERSION \
-    --namespace kolla --name cinder-create-keystone-endpoint-admin --set "$common_vars"
-
-helm install kolla/cinder-create-keystone-endpoint-adminv2-job --version $VERSION \
-    --namespace kolla --name cinder-create-keystone-endpoint-adminv2 --set "$common_vars"
 
 for x in nova nova-api; do
     helm install kolla/$x-create-db-job --version $VERSION \
@@ -188,11 +149,11 @@ $DIR/tests/bin/endpoint_test.sh
 [ -d "$WORKSPACE/logs" ] && openstack catalog list > \
     $WORKSPACE/logs/openstack-catalog-after-bootstrap.json || true
 
-for x in nova nova-api cinder; do
+for x in nova nova-api; do
     helm delete --purge $x-create-db
 done
 
-for x in nova-api cinder; do
+for x in nova-api; do
     helm delete --purge $x-manage-db
 done
 
@@ -200,36 +161,20 @@ kollakube res delete bootstrap \
     nova-create-keystone-endpoint-internal \
     nova-create-keystone-endpoint-admin \
 
-for x in cinder; do
-    helm delete --purge $x-create-keystone-service
-    helm delete --purge $x-create-keystone-endpoint-public
-    helm delete --purge $x-create-keystone-endpoint-internal
-    helm delete --purge $x-create-keystone-endpoint-admin
-done
-
-helm delete --purge cinder-create-keystone-servicev2
-helm delete --purge cinder-create-keystone-endpoint-publicv2
-helm delete --purge cinder-create-keystone-endpoint-internalv2
-helm delete --purge cinder-create-keystone-endpoint-adminv2
-
 helm install kolla/cinder-volume-ceph-statefulset --version $VERSION \
     --set "$common_vars,element_name=cinder" --namespace kolla \
     --name cinder-volume-ceph-statefulset
 
-helm install kolla/cinder-api-deployment --version $VERSION \
-    --set "$common_vars" --namespace kolla \
-    --name cinder-api
-
-helm install kolla/cinder-scheduler-statefulset --version $VERSION \
-    --set "$common_vars,element_name=cinder-scheduler" \
-    --namespace kolla --name cinder-scheduler
+helm install kolla/cinder-control --debug --version $VERSION \
+    --namespace kolla --name cinder --set "$common_vars,element_name=cinder" \
+    --values <(helm_entrypoint_general $1)
 
 helm install kolla/glance --version $VERSION \
     --namespace kolla --name glance --set "$common_vars,element_name=glance" \
     --values <(helm_entrypoint_general $1)
 
 helm install kolla/openvswitch --version $VERSION \
---namespace kolla --name openvswitch --values  <(helm_entrypoint_general)
+--namespace kolla --name openvswitch --values  <(helm_entrypoint_general $1)
 
 $DIR/tools/pull_containers.sh kolla
 $DIR/tools/wait_for_pods.sh kolla
