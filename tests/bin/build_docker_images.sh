ee4ce1f500083a41c0e296a3d9f6e3da50768b9b
Kevin Fox <kevin@efox.cc>
2017-01-27 07:19:35 -0800

    Periodic gate fix #2
    The previous PS made progress to fixing the periodic builds but
    had two remaining issues. The file format needs ':'s instead of
    spaces. The bzip2 binary is also bzip2, not bzip.
    
    Change-Id: I60dc7edafe44a73b5c68fb6928afb39feea0be5a

--- tests/bin/build_docker_images.sh
+++ tests/bin/build_docker_images.sh
@@ -3,16 +3,16 @@ DISTRO="$2"
 TYPE="$3"
 CONFIG="$4"
 PIPELINE="$5"
-sudo docker images | awk '{if($1!="REPOSITORY"){print $1, $2}}' | sort -u > $LOGS/docker_images.txt
+sudo docker images | awk '{if($1!="REPOSITORY"){print $1 ":" $2}}' | sort -u > $LOGS/docker_images.txt
 cat $LOGS/docker_images.txt | grep kolla | sed 's@^kolla@docker.io/kolla@' | sort -u > $LOGS/docker_kolla_images.txt
 cat $LOGS/docker_images.txt | grep -v kolla | sed 's@^kolla@docker.io/kolla@' | sort -u > $LOGS/docker_kubernetes_images.txt
 
 if [ "x$PIPELINE" == "xperiodic" ]; then
     sudo docker save -o $WORKSPACE/UPLOAD_CONTAINERS/kubernetes.tar $(cat $LOGS/docker_kubernetes_images.txt)
-    bzip $WORKSPACE/UPLOAD_CONTAINERS/kubernetes.tar
+    bzip2 $WORKSPACE/UPLOAD_CONTAINERS/kubernetes.tar
     cp $LOGS/docker_kubernetes_images.txt $WORKSPACE/UPLOAD_CONTAINERS/kubernetes-containers.txt
     sudo docker save -o $WORKSPACE/UPLOAD_CONTAINERS/$DISTRO-$TYPE-$CONFIG.tar $(cat $LOGS/docker_kolla_images.txt)
-    bzip $WORKSPACE/UPLOAD_CONTAINERS/$DISTRO-$TYPE-$CONFIG.tar
+    bzip2 $WORKSPACE/UPLOAD_CONTAINERS/$DISTRO-$TYPE-$CONFIG.tar
     cp $LOGS/docker_kubernetes_images.txt $WORKSPACE/UPLOAD_CONTAINERS/$DISTRO-$TYPE-$CONFIG-containers.txt
     sudo chown $USER $WORKSPACE/UPLOAD_CONTAINERS/*
 fi
