7bedb2ae6a7d315ba36b862600bd3c0d8a5223c1
Serguei Bezverkhi <sbezverk@cisco.com>
2017-01-27 19:31:57 -0500

    PS adds glance cleanup service
    PS adds glance cleanup service
    
    Change-Id: I885e39c1b184ac1fd73a51d694dd147011d0dc51
    Partially-Implements: blueprint helm-service-cleanup

--- tests/bin/ceph_workflow.sh
+++ tests/bin/ceph_workflow.sh
@@ -34,7 +34,7 @@ function general_config {
     common_workflow_config $IP $base_distro $tunnel_interface
 }
 
-common_vars="kube_logger=false,base_distro=$base_distro"
+common_vars="kube_logger=false,base_distro=$base_distro,grace_period_seconds=0"
 
 kollakube res create configmap \
     mariadb keystone horizon rabbitmq memcached nova-api nova-conductor \
