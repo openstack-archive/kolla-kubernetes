feec41d69d6ad8b5056dac93c9a3f53c7773a6da
Serguei Bezverkhi <sbezverk@cisco.com>
2017-01-27 19:31:57 -0500

    PS adds glance cleanup service
    PS adds glance cleanup service
    
    Change-Id: I885e39c1b184ac1fd73a51d694dd147011d0dc51
    Partially-Implements: blueprint helm-service-cleanup

---
+++ tools/wait_for_pods_termination.sh
@@ -0,0 +1,16 @@
+#!/bin/bash -e
+set +x
+end=$(date +%s)
+end=$((end + 300))
+while true; do
+    kubectl get pods --namespace=$1 | grep Terminating > /dev/null && \
+        TERMINATING=True || TERMINATING=False
+    [ $TERMINATING == "False" ] && \
+        break || true
+    sleep 1
+    now=$(date +%s)
+    echo 'Waitng for pod to terminate: ' $now
+    [ $now -gt $end ] && echo containers failed to terminate. && \
+        kubectl get pods --namespace $1 && exit -1
+done
+set -x
