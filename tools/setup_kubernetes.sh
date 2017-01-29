50bc6f864a9a4b831b09f166db4e5377fbf49d1e
Kevin Fox <kevin@efox.cc>
2017-01-29 11:27:33 -0800

    Pull container tallballs into the gate
    Pull containers from tarballs.oo to make gate more reliable.
    
    This patch works by downloading the regitry from tarballs.oo, then
    using the docker load operation to instantiate the tarballs.  Note
    we use wget in this patch instead of curl as wget is far more reliable.
    
    Change-Id: Ibc92755f20c4f4fcc1a0e13d7b2d58967a9dd5e9

--- tools/setup_kubernetes.sh
+++ tools/setup_kubernetes.sh
@@ -13,7 +13,7 @@ repo_gpgcheck=1
 gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg
        https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
 EOEF
-yum install -y docker kubelet kubeadm kubectl kubernetes-cni ebtables
+yum install -y docker kubelet kubeadm kubectl kubernetes-cni ebtables bzip2
 systemctl start kubelet
 EOF
 else
@@ -22,14 +22,28 @@ apt-get install -y apt-transport-https
 curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
 echo "deb http://apt.kubernetes.io/ kubernetes-xenial main" > /etc/apt/sources.list.d/kubernetes.list
 apt-get update
-apt-get install -y docker.io kubelet kubeadm kubectl kubernetes-cni
+apt-get install -y docker.io kubelet kubeadm kubectl kubernetes-cni bzip2
 EOF
 fi
 cat >> /tmp/setup.$$ <<"EOF"
 systemctl start docker
 EOF
+
+sudo bash /tmp/setup.$$
+sudo docker ps -a
+
+if [ "x$WORKSPACE" == "x" ]; then
+    WORKSPACE="$HOME"
+fi
+if [ -d "$WORKSPACE/DOWNLOAD_CONTAINERS" ]; then
+    ls "$WORKSPACE/DOWNLOAD_CONTAINERS"/*.bz2 | sudo xargs -I bunzip2 {}
+    ls "$WORKSPACE/DOWNLOAD_CONTAINERS"/*.tar | sudo xargs -I docker load -i {}
+    rm -f "$WORKSPACE/DOWNLOAD_CONTAINERS"/*.tar
+fi
+sudo docker images
+
 if [ "$1" == "master" ]; then
-    cat >> /tmp/setup.$$ <<"EOF"
+    cat > /tmp/setup.$$ <<"EOF"
 [ -d /etc/kubernetes/manifests ] && rmdir /etc/kubernetes/manifests || true
 kubeadm init --skip-preflight-checks --service-cidr 172.16.128.0/24 --api-advertise-addresses $(cat /etc/nodepool/primary_node_private) | tee /tmp/kubeout
 grep 'kubeadm join --token' /tmp/kubeout | awk '{print $3}' | sed 's/[^=]*=//' > /etc/kubernetes/token.txt
@@ -37,7 +51,7 @@ grep 'kubeadm join --token' /tmp/kubeout | awk '{print $4}' > /etc/kubernetes/ip
 rm -f /tmp/kubeout
 EOF
 else
-    cat >> /tmp/setup.$$ <<EOF
+    cat > /tmp/setup.$$ <<EOF
 kubeadm join --token "$2" "$3" --skip-preflight-checks
 EOF
 fi
