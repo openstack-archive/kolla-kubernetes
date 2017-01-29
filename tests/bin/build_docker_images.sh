777b7ef702aa903675dd8ce42327d331bb0fcc76
Kevin Fox <kevin@efox.cc>
2017-01-28 17:31:55 -0800

    Periodic job fix 3
    The previous PS's for the periodic jobs managed to successfully
    build and upload the docker tarballs, but failed to compress it
    due to an ordering issue. This PS fixes the order.
    
    Change-Id: I752f753c71dfd403770f390ec84f03556d8a7ae7

--- tests/bin/build_docker_images.sh
+++ tests/bin/build_docker_images.sh
@@ -9,10 +9,10 @@ cat $LOGS/docker_images.txt | grep -v kolla | sed 's@^kolla@docker.io/kolla@' | 
 
 if [ "x$PIPELINE" == "xperiodic" ]; then
     sudo docker save -o $WORKSPACE/UPLOAD_CONTAINERS/kubernetes.tar $(cat $LOGS/docker_kubernetes_images.txt)
-    bzip2 $WORKSPACE/UPLOAD_CONTAINERS/kubernetes.tar
-    cp $LOGS/docker_kubernetes_images.txt $WORKSPACE/UPLOAD_CONTAINERS/kubernetes-containers.txt
     sudo docker save -o $WORKSPACE/UPLOAD_CONTAINERS/$DISTRO-$TYPE-$CONFIG.tar $(cat $LOGS/docker_kolla_images.txt)
-    bzip2 $WORKSPACE/UPLOAD_CONTAINERS/$DISTRO-$TYPE-$CONFIG.tar
-    cp $LOGS/docker_kubernetes_images.txt $WORKSPACE/UPLOAD_CONTAINERS/$DISTRO-$TYPE-$CONFIG-containers.txt
     sudo chown $USER $WORKSPACE/UPLOAD_CONTAINERS/*
+    bzip2 $WORKSPACE/UPLOAD_CONTAINERS/kubernetes.tar
+    bzip2 $WORKSPACE/UPLOAD_CONTAINERS/$DISTRO-$TYPE-$CONFIG.tar
+    cp $LOGS/docker_kubernetes_images.txt $WORKSPACE/UPLOAD_CONTAINERS/kubernetes-containers.txt
+    cp $LOGS/docker_kubernetes_images.txt $WORKSPACE/UPLOAD_CONTAINERS/$DISTRO-$TYPE-$CONFIG-containers.txt
 fi
