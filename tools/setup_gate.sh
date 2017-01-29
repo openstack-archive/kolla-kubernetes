1a4efb10752699b0509afbad9ab28e22592ddb86
Kevin Fox <kevin@efox.cc>
2017-01-29 11:27:33 -0800

    Pull container tallballs into the gate
    Pull containers from tarballs.oo to make gate more reliable.
    
    This patch works by downloading the regitry from tarballs.oo, then
    using the docker load operation to instantiate the tarballs.  Note
    we use wget in this patch instead of curl as wget is far more reliable.
    
    Depends-On: I7dcb6b2a8faf1ce74e10f73f6465f4c693031990
    Change-Id: Ibc92755f20c4f4fcc1a0e13d7b2d58967a9dd5e9

--- tools/setup_gate.sh
+++ tools/setup_gate.sh
@@ -11,14 +11,69 @@ trap 'tests/bin/gate_capture_logs.sh "$?"' ERR
 mkdir -p $WORKSPACE/logs/
 env > $WORKSPACE/logs/env
 
-if [ "x$PIPELINE" == "xperiodic" ]; then
-    mkdir -p $WORKSPACE/UPLOAD_CONTAINERS
-fi
+# TODO(sdake): install wget via ansible as per kolla-ansible deliverable
+#              gating tools in the future
+function install_wget {
+    # NOTE(sdake) wget is far more reliable than curl
+    if [ "$DISTRO" == "centos" -o "$DISTRO" == "oraclelinux" ]; then
+        sudo yum -y install wget
+    else
+        sudo apt-get -y install wget
+   fi
+}
+
+function prepare_images {
+    if [ "x$PIPELINE" != "xperiodic" ]; then
+        C=$CONFIG
+        if [ "x$CONFIG" == "xexternal-ovs" -o "x$CONFIG" == "xceph-multi" -o \
+            "x$CONFIG" == "xhelm-entrypoint" -o "x$CONFIG" == "xhelm-operator" \
+            ]; then
+            C="ceph"
+        fi
+    fi
+    mkdir -p $WORKSPACE/DOWNLOAD_CONTAINERS
+    BASE_URL=http://tarballs.openstack.org/kolla-kubernetes/gate/containers
+
+    # TODO(sdake): Cross-repo depends-on is completely broken
+
+    FILENAME="$DISTRO-$TYPE-$BRANCH-$C"
+
+    # NOTE(sdake): This includes both a set of kubernetes containers
+    #              for running kubernetes infrastructure as well as
+    #              kolla containers for 2.0.2 and 3.0.2.  master images
+    #              are not yet available via this mechanism.
+
+    # NOTE(sdake): Obtain pre-built containers to load into docker
+    #              via docker load
+
+    wget -q -c -O \
+        "$WORKSPACE/DOWNLOAD_CONTAINERS/$FILENAME" \
+        "$BASE_URL/$FILENAME.tar.bz2"
+    wget -q -c -O \
+          "$WORKSPACE/DOWNLOAD_CONTAINERS/kubernetes.tar.gz" \
+        "$BASE_URL/kubernetes.tar.bz2"
+
+    # NOTE(sdake): Obtain lists of containers
+    wget -q -c -O \
+        "$WORKSPACE/DOWNLOAD_CONTAINERS/$FILENAME-containers.txt" \
+        "$BASE_URL/$FILENAME-containers.txt"
+    wget -q -c -O \
+        "$WORKSPACE/DOWNLOAD_CONTAINERS/kubernetes-containers.txt" \
+        "$BASE_URL/kubernetes-containers.txt"
+}
 
 if [ "x$BRANCH" == "xt" ]; then
     echo Version: $BRANCH is not implemented yet.
     exit -1
 fi
+
+# NOTE(sdake): This seems disturbing (see note at end of file)
+if [ "x$PIPELINE" == "xperiodic" ]; then
+    mkdir -p $WORKSPACE/UPLOAD_CONTAINERS
+fi
+
+install_wget
+prepare_images
 
 if [ "x$BRANCH" == "x3" ]; then
     sed -i 's/2\.0\.2/3.0.2/g' helm/all_values.yaml
