536bc0e64ba0d036dcfe5d4c4f992e176605afa2
Kevin Fox <kevin@efox.cc>
2017-01-30 10:30:45 -0800

    WIP: Helm template testing
    Change-Id: I24a70fb4dbdbf06f8340abe3b7e72f794e09a5d9

--- tools/test.sh
+++ tools/test.sh
@@ -3,4 +3,14 @@
 grep api_interface_address ../kolla/etc/kolla/globals.yml || echo api_interface_address: "0.0.0.0" >> ../kolla/etc/kolla/globals.yml
 grep tunnel_interface_address ../kolla/etc/kolla/globals.yml || echo tunnel_interface_address: "0.0.0.0" >> ../kolla/etc/kolla/globals.yml
 grep orchestration_engine ../kolla/etc/kolla/globals.yml || echo orchestration_engine: KUBERNETES >> ../kolla/etc/kolla/globals.yml
+#sudo yum install -y golang-bin || sudo apt-get install -y golang
+#tools/build_helm_templates.sh
+mkdir -p ~/.helm/plugins/template
+curl -o ~/.helm/plugins/template/tpl http://www.efox.cc/temp/tpl
+curl -o ~/.helm/plugins/template/plugin.yaml http://www.efox.cc/temp/plugin.yaml
+curl -o /tmp/helm.tar.gz https://storage.googleapis.com/kubernetes-helm/helm-v2.1.3-linux-amd64.tar.gz
+mkdir -p ~/bin
+tar --strip-components 1 -C ~/bin linux-amd64/helm -zxf -
+export PATH=$PATH:~/bin
+helm template || true
 python setup.py test --slowest --testr-args="$1"
