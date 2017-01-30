5edd8130c0d71177f609e58f2938d2cd0d0ba216
Kevin Fox <kevin@efox.cc>
2017-01-30 10:30:45 -0800

    WIP: Helm template testing
    Change-Id: I24a70fb4dbdbf06f8340abe3b7e72f794e09a5d9

--- tools/test.sh
+++ tools/test.sh
@@ -3,4 +3,22 @@
 grep api_interface_address ../kolla/etc/kolla/globals.yml || echo api_interface_address: "0.0.0.0" >> ../kolla/etc/kolla/globals.yml
 grep tunnel_interface_address ../kolla/etc/kolla/globals.yml || echo tunnel_interface_address: "0.0.0.0" >> ../kolla/etc/kolla/globals.yml
 grep orchestration_engine ../kolla/etc/kolla/globals.yml || echo orchestration_engine: KUBERNETES >> ../kolla/etc/kolla/globals.yml
+#sudo yum install -y golang-bin || sudo apt-get install -y golang
+#tools/build_helm_templates.sh
+set -x
+mkdir -p ~/.helm/plugins/template
+curl -L -o /tmp/helm-template.tar.gz https://github.com/technosophos/helm-template/releases/download/2.1.3%2B1/helm-template-linux.tgz
+curl -L -o /tmp/helm.tar.gz https://storage.googleapis.com/kubernetes-helm/helm-v2.1.3-linux-amd64.tar.gz
+mkdir -p ~/bin
+tar --strip-components 1 -C ~/bin linux-amd64/helm -zxf /tmp/helm.tar.gz
+tar -C ~/.helm/plugins/template/ -zxf /tmp/helm-template.tar.gz
+export PATH=$PATH:~/bin
+export HOME=$(cd ~; pwd)
+export HELMDIR=$(pwd)/helm
+export HELMBIN=$HOME/bin/helm
+export HELM_HOME=$HOME/.helm/
+helm init -c
+helm template || true
+tools/helm_build_all.sh /tmp/repo.$$
+tools/helm_buildrepo.sh /tmp/repo.$$ &
 python setup.py test --slowest --testr-args="$1"
