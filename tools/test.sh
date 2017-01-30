3126a9bce74521ab20d498b271b02e17a209a9b3
Kevin Fox <kevin@efox.cc>
2017-01-30 10:30:45 -0800

    WIP: Helm template testing
    Change-Id: I24a70fb4dbdbf06f8340abe3b7e72f794e09a5d9

--- tools/test.sh
+++ tools/test.sh
@@ -3,4 +3,6 @@
 grep api_interface_address ../kolla/etc/kolla/globals.yml || echo api_interface_address: "0.0.0.0" >> ../kolla/etc/kolla/globals.yml
 grep tunnel_interface_address ../kolla/etc/kolla/globals.yml || echo tunnel_interface_address: "0.0.0.0" >> ../kolla/etc/kolla/globals.yml
 grep orchestration_engine ../kolla/etc/kolla/globals.yml || echo orchestration_engine: KUBERNETES >> ../kolla/etc/kolla/globals.yml
+sudo yum install -y golang-bin || sudo apt-get install -y golang
+tools/build_helm_templates.sh
 python setup.py test --slowest --testr-args="$1"
