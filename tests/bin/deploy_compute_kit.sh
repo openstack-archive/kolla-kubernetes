95f622fbe8f05bdb05348d63e0d0c7df2683fffa
Serguei Bezverkhi <sbezverk@cisco.com>
2017-02-02 09:04:52 -0500

    WIP Adding compute-kit chart
    This chart will deploy openstack components using services charts.
    
    Change-Id: I7dcb6b2a8faf1ce74e10f73f6465f4c693031990
    Partially-Implements: blueprint helm-meta

---
+++ tests/bin/deploy_compute_kit.sh
@@ -0,0 +1,21 @@
+#!/bin/bash -xe
+
+VERSION=0.4.0-1
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/../.." && pwd )"
+IP=172.18.0.1
+
+. "$DIR/tests/bin/common_entrypoint_config.sh"
+
+function entrypoint_config {
+    common_entrypoint_config $IP $base_distro $tunnel_interface $1
+}
+
+tunnel_interface=docker0
+base_distro="$2"
+
+helm install kolla/compute-kit-0.4.0 --version $VERSION \
+    --namespace kolla --name compute-kit-0.4.0 \
+    --values <(entrypoint_config)
+
+$DIR/tools/wait_for_pods.sh kolla 600
