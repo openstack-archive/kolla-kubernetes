6053245d109823f9b62d3186d0572274ee2ddcb1
Borne Mace <borne.mace@oracle.com>
2017-02-03 11:21:02 -0800

    Added support for containerized dev bringup
    The intent of this change is to bring up kolla-kubernetes
    on top of a base / fresh halcyon-vagrant-kubernetes environment.
    Some assumptions at this point include the base environment
    is using centos and ceph.  The scripts need to be improved to
    handle failures and re-runs.
    
    Change-Id: I49f332b97b41be8098b0141696c2ad205e231b00

--- tools/build_local_admin_keystonerc.sh
+++ tools/build_local_admin_keystonerc.sh
@@ -1,7 +1,9 @@
 #!/bin/bash -e
 
-KEYSTONE_CLUSTER_IP=`kubectl get svc keystone-public --namespace=kolla -o \
-    jsonpath='{.spec.clusterIP}'`
+KEYSTONE_ADMIN_IP=`kubectl get svc keystone-admin --namespace=kolla -o \
+    jsonpath='{.spec.externalIPs[0]}'`
+KEYSTONE_ADMIN_PORT=`kubectl get svc keystone-admin --namespace=kolla -o \
+    jsonpath='{.spec.ports[0].port}'`
 KEYSTONE_ADMIN_PASSWD=`grep keystone_admin_password /etc/kolla/passwords.yml \
     | cut -d':' -f2 | sed -e 's/ //'`
 
@@ -9,7 +11,7 @@ cat > ~/keystonerc_admin <<EOF
 unset OS_SERVICE_TOKEN
 export OS_USERNAME=admin
 export OS_PASSWORD=$KEYSTONE_ADMIN_PASSWD
-export OS_AUTH_URL=http://$KEYSTONE_CLUSTER_IP:5000/v3
+export OS_AUTH_URL=http://$KEYSTONE_ADMIN_IP:$KEYSTONE_ADMIN_PORT/v3
 export PS1='[\u@\h \W(keystone_admin)]$ '
 export OS_PROJECT_NAME=admin
 export OS_USER_DOMAIN_NAME=Default
