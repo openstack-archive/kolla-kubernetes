8f99d48f3f81af97bb76dbac3ac1a17191c9d320
Steven Dake <stdake@cisco.com>
2017-02-08 13:30:16 -0700

    Change endpoint-test -1 to 1 return code
    POSIX and bash in general prefer 0> error codes.  The -1 error
    codes are casted to an unsigned value creating some misery in
    decoding return codes.
    
    Change-Id: I5b3b8f95d385c62c774da662bb52d7951b69b9a7

--- tests/bin/endpoint_test.sh
+++ tests/bin/endpoint_test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 
-# This bash exits 0< during a failure and 0 during success.  Possible
+# This bash exits 0> during a failure and 0 during success.  Possible
 # error conditions include:
 # * failure to curl the AuthToken from OpenStack
 # * failure to find the endpoints in the curled result
@@ -9,13 +9,13 @@
 
 function endpoints_dump_and_fail {
     cat /tmp/$$.1
-    exit -1
+    exit 1
 }
 
 OS_TOKEN=$(openstack token issue -f value -c id)
 curl -H "X-Auth-Token:$OS_TOKEN" $OS_AUTH_URL/endpoints -o /tmp/$$
 jq -r '.endpoints[] | .service_id' /tmp/$$ | sort | uniq -c > /tmp/$$.1
-awk '{if($1 != 3){exit -1}}' /tmp/$$.1 || endpoints_dump_and_fail
+awk '{if($1 != 3){exit 1}}' /tmp/$$.1 || endpoints_dump_and_fail
 
 # If the workspace isn't set (because this is the dev env),  unconditionally
 # exit 0
