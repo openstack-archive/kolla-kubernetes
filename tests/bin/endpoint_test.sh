9f776d4069d026e7e061f4e6dc8803760ee25999
Steven Dake <stdake@cisco.com>
2017-02-08 12:10:11 -0700

    Document the magic in endpoint_test.sh
    Also unconditionally return 0 if the WORKSPACE environment variable
    isn't set.  If it is set return the return code from the various cp
    operations.
    
    Change-Id: I2ac576e6a1614b200eb52a1a8bf6737ecfd99f2c

--- tests/bin/endpoint_test.sh
+++ tests/bin/endpoint_test.sh
@@ -1,5 +1,10 @@
 #!/bin/bash -e
 
+# NOTE(sdake) This bash exits -1 during a failure. During the dev
+              environment setup, cp may is failing resulting in a >0 error
+              code returned to the parent script.  This causes the dev
+              env to crater.
+
 function endpoints_dump_and_fail {
     cat /tmp/$$.1
     exit -1
@@ -11,3 +16,14 @@ jq -r '.endpoints[] | .service_id' /tmp/$$ | sort | uniq -c > /tmp/$$.1
 awk '{if($1 != 3){exit -1}}' /tmp/$$.1 || endpoints_dump_and_fail
 [ -d $WORKSPACE/logs ] && cp /tmp/$$ $WORKSPACE/logs/endpoints.txt
 [ -d $WORKSPACE/logs ] && cp /tmp/$$.1 $WORKSPACE/logs/endpoints1.txt
+
+EXITCODE = "$?"
+
+# NOTE(sdake): If the workspace isn't set (because this is the dev env),
+               unconditionally return 0
+
+if [ "x$WORKSPACE" == "x" ]; then
+    return 0
+fi
+
+exit EXITCODE
