cb07057115f0abc0d94b11b0ab5f7c202758d86b
Serguei Bezverkhi <sbezverk@cisco.com>
2017-02-09 08:27:17 -0500

    Adding compute kit based on iscsi backend
    Adding compute kit based on iscsi backend and experimental job.
    
    Partially-Implements: blueprint helm-meta
    
    Change-Id: Ia553f67e71ed1a39aadaa1a2f3274dbfdf0d61a4

--- tests/bin/deploy_compute_kit.sh
+++ tests/bin/deploy_compute_kit.sh
@@ -8,17 +8,26 @@ IP=172.18.0.1
 . "$DIR/tests/bin/common_workflow_config.sh"
 . "$DIR/tests/bin/common_iscsi_config.sh"
 
-function general_config {
-    common_workflow_config $IP $base_distro $tunnel_interface
-
-function entrypoint_config {
-    general_config
-    common_iscsi_config
-}
-
 tunnel_interface=docker0
 base_distro="$2"
 
-echo "Not yet implemented. Exiting ..."
+function general_config {
+    common_workflow_config $IP $base_distro $tunnel_interface
+}
 
-exit 1
+function iscsi_config {
+    common_iscsi_config
+}
+
+common_vars="ceph_backend=false,kube_logger=false,base_distro=$base_distro,global.kolla.keystone.all.admin_port_external=true"
+
+general_config > /tmp/general_config.yaml
+iscsi_config > /tmp/iscsi_config.yaml
+
+helm install kolla/compute-kit-0.5.0 --version $VERSION \
+    --namespace kolla --name compute-kit-0.5.0 \
+    --values /tmp/general_config.yaml --values /tmp/iscsi_config.yaml
+
+$DIR/tools/wait_for_pods.sh kolla 900
+
+$DIR/tools/build_local_admin_keystonerc.sh
