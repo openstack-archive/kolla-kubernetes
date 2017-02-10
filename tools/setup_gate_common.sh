632eb393ba25f4356ee2cd5225dc8af0dc45df3b
Serguei Bezverkhi <sbezverk@cisco.com>
2017-02-10 13:09:58 -0500

    DO NOT MERGE git bisect for b3 failure
    DO NOT MERGE git bisect for b3 failure
    
    Change-Id: I0f35392bb162578719386922cab39a9841f3d0fd

--- tools/setup_gate_common.sh
+++ tools/setup_gate_common.sh
@@ -55,6 +55,10 @@ EOF
     openstack/kolla-ansible && true
 [ ! -d kolla-ansible ] && git clone https://github.com/openstack/kolla-ansible.git
 
+pushd kolla-ansible;
+git checkout 627719e66cae10d9673b786516b4a93ed65fc6cb 
+popd
+
 sudo ln -s `pwd`/kolla-ansible/etc/kolla /etc/kolla
 sudo ln -s `pwd`/kolla-ansible /usr/share/kolla
 sudo ln -s `pwd`/etc/kolla-kubernetes /etc/kolla-kubernetes
