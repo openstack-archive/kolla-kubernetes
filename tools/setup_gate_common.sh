4a2b3da2082a70372321736f9ad2641aace63263
Steven Dake <stdake@cisco.com>
2017-02-11 13:24:37 -0700

    DNM Kolla ansible broken
    git bisect start
    git bisect good cac3ed855c5dd4b059f0bebf58dcbaf486817879
    git bisect bad master
    
    Change-Id: I4ff01bd98c2637fd89af31ef04d2e798192812b1

--- tools/setup_gate_common.sh
+++ tools/setup_gate_common.sh
@@ -55,6 +55,10 @@ EOF
     openstack/kolla-ansible && true
 [ ! -d kolla-ansible ] && git clone https://github.com/openstack/kolla-ansible.git
 
+pushd kolla-ansible;
+git checkout 000e850933ded0ed795cec8f87be6a537487660e
+popd
+
 sudo ln -s `pwd`/kolla-ansible/etc/kolla /etc/kolla
 sudo ln -s `pwd`/kolla-ansible /usr/share/kolla
 sudo ln -s `pwd`/etc/kolla-kubernetes /etc/kolla-kubernetes
