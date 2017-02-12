2c7809b47ecbdef40a62c00d99c5955976537c0b
Serguei Bezverkhi <sbezverk@cisco.com>
2017-02-12 12:59:16 -0500

    DO NOT MERGE: Hunting for canal plugin failure
    DO NOT MERGE: Hunting for canal plugin failure
    
    Change-Id: I8747735e8a69273194df5f66c036b7d9e0ca2a4b

--- tests/bin/setup_canal.sh
+++ tests/bin/setup_canal.sh
@@ -10,6 +10,10 @@ curl "$url" -o /tmp/canal.yaml
 sed -i "s@192.168.0.0/16@172.16.130.0/22@" /tmp/canal.yaml
 sed -i "s@10.96.232.136@172.16.128.100@" /tmp/canal.yaml
 
+kubectl get pods -n kube-system
+kubectl get nodes
+kubectl get nodes | grep -v NAME | awk '{print $1}' | xargs -l kubectl describe node
+
 kubectl create -f /tmp/canal.yaml
 
 $DIR/tools/pull_containers.sh kube-system
