1e70038993a32c7111ba3f83611683a824f441da
Serguei Bezverkhi <sbezverk@cisco.com>
2017-02-13 16:36:10 -0500

    WIP adding Ironic microservices packages
    WIP adding Ironic microservices packages
    
    Change-Id: I73ca794fe37c44325ad83f5b312ca14833f1b0ba
    Partially-Implements: blueprint helm-microservices

--- tools/setup_gate_iscsi.sh
+++ tools/setup_gate_iscsi.sh
@@ -52,7 +52,9 @@ kollakube res create configmap \
     openvswitch-vswitchd nova-libvirt nova-compute nova-consoleauth \
     nova-novncproxy nova-novncproxy-haproxy neutron-server-haproxy \
     nova-api-haproxy cinder-api cinder-api-haproxy cinder-backup \
-    cinder-scheduler cinder-volume iscsid tgtd keepalived;
+    cinder-scheduler cinder-volume iscsid tgtd keepalived \
+    ironic-api ironic-api-haproxy ironic-conductor ironic-dnsmasq \
+    ironic-inspector ironic-inspector-haproxy;
 kollakube res create secret nova-libvirt
 
 if [ "x$4" == "xhelm-compute-kit" ]; then
@@ -62,6 +64,11 @@ else
 fi
 
 . ~/keystonerc_admin
+
+kubectl get pods -n kolla | grep ironic
+kubectl get svc -n kolla | grep ironic
+kubectl get configmaps -n kolla | grep ironic
+openstack baremetal node list
 
 sudo pvs >> $WORKSPACE/logs/pvs.txt
 
