772fd86e70edf131342504cf8e9e622818094d54
Serguei Bezverkhi <sbezverk@cisco.com>
2017-02-16 14:01:58 -0500

    Add nova-compute-ironic microservice chart
    To be able to use ironic in kolla-kubernetes, microservice chart
    for nova-compute-ironic process is required. Also adds
    ironic control plane tests.
    
    Partially_implements: blueprint helm-microservices
    
    Change-Id: I8a9bb1b4192033416809d687ed6d6ac56ecd8e3f

--- tests/bin/iscsi_workflow.sh
+++ tests/bin/iscsi_workflow.sh
@@ -412,56 +412,60 @@ helm install kolla/tgtd-daemonset --version $VERSION \
 #
 # Ironic related activities
 #
-helm install kolla/ironic-api-svc --version $VERSION \
-    --namespace kolla --name ironic-api-svc \
-    --values /tmp/general_config.yaml --values /tmp/iscsi_config.yaml
+#helm install kolla/ironic-api-svc --version $VERSION \
+#    --namespace kolla --name ironic-api-svc \
+#    --values /tmp/general_config.yaml --values /tmp/iscsi_config.yaml
 
-helm install kolla/ironic-create-keystone-service-job --version $VERSION \
-    --namespace kolla --name ironic-create-keystone-service \
-    --values /tmp/general_config.yaml --values /tmp/iscsi_config.yaml
+#helm install kolla/ironic-create-keystone-service-job --version $VERSION \
+#    --namespace kolla --name ironic-create-keystone-service \
+#    --values /tmp/general_config.yaml --values /tmp/iscsi_config.yaml
 
-helm install kolla/ironic-create-keystone-user-job --version $VERSION \
-    --namespace kolla --name ironic-create-keystone-user \
-    --values /tmp/general_config.yaml --values /tmp/iscsi_config.yaml
-
-$DIR/tools/wait_for_pods.sh kolla
-
-helm delete --purge ironic-create-keystone-user
-helm delete --purge ironic-create-keystone-service
-
-helm install kolla/ironic-api-create-db-job --version $VERSION \
-    --namespace kolla --name ironic-api-create-db \
-    --values /tmp/general_config.yaml --values /tmp/iscsi_config.yaml
+#helm install kolla/ironic-create-keystone-user-job --version $VERSION \
+#    --namespace kolla --name ironic-create-keystone-user \
+#    --values /tmp/general_config.yaml --values /tmp/iscsi_config.yaml
 
 $DIR/tools/wait_for_pods.sh kolla
 
-helm install kolla/ironic-api-manage-db-job --version $VERSION \
-    --namespace kolla --name ironic-api-manage-db \
-    --values /tmp/general_config.yaml --values /tmp/iscsi_config.yaml
+#helm delete --purge ironic-create-keystone-user
+#helm delete --purge ironic-create-keystone-service
 
-helm install kolla/ironic-api-create-keystone-endpoint-internal-job --version $VERSION \
-    --namespace kolla --name ironic-api-create-keystone-endpoint-internal \
-    --values /tmp/general_config.yaml --values /tmp/iscsi_config.yaml
-
-helm install kolla/ironic-api-create-keystone-endpoint-admin-job --version $VERSION \
-    --namespace kolla --name ironic-api-create-keystone-endpoint-admin \
-    --values /tmp/general_config.yaml --values /tmp/iscsi_config.yaml
-
-helm install kolla/ironic-api-create-keystone-endpoint-public-job --version $VERSION \
-    --namespace kolla --name ironic-api-create-keystone-endpoint-public \
-    --values /tmp/general_config.yaml --values /tmp/iscsi_config.yaml
+#helm install kolla/ironic-api-create-db-job --version $VERSION \
+#    --namespace kolla --name ironic-api-create-db \
+#    --values /tmp/general_config.yaml --values /tmp/iscsi_config.yaml
 
 $DIR/tools/wait_for_pods.sh kolla
 
-helm install kolla/ironic-api-deployment --debug --version $VERSION \
-    --namespace kolla --name ironic-api-deployment \
-    --values /tmp/general_config.yaml --values /tmp/iscsi_config.yaml
+#helm install kolla/ironic-api-manage-db-job --version $VERSION \
+#    --namespace kolla --name ironic-api-manage-db \
+#    --values /tmp/general_config.yaml --values /tmp/iscsi_config.yaml
+
+#helm install kolla/ironic-api-create-keystone-endpoint-internal-job --version $VERSION \
+#    --namespace kolla --name ironic-api-create-keystone-endpoint-internal \
+#    --values /tmp/general_config.yaml --values /tmp/iscsi_config.yaml
+
+#helm install kolla/ironic-api-create-keystone-endpoint-admin-job --version $VERSION \
+#    --namespace kolla --name ironic-api-create-keystone-endpoint-admin \
+#    --values /tmp/general_config.yaml --values /tmp/iscsi_config.yaml
+
+#helm install kolla/ironic-api-create-keystone-endpoint-public-job --version $VERSION \
+#    --namespace kolla --name ironic-api-create-keystone-endpoint-public \
+#    --values /tmp/general_config.yaml --values /tmp/iscsi_config.yaml
 
 $DIR/tools/wait_for_pods.sh kolla
 
-helm install kolla/ironic-conductor-daemonset --debug --version $VERSION \
-    --namespace kolla --name ironic-conductor-daemonset \
-    --values /tmp/general_config.yaml --values /tmp/iscsi_config.yaml
+#helm install kolla/ironic-api-deployment --version $VERSION \
+#    --namespace kolla --name ironic-api-deployment \
+#    --values /tmp/general_config.yaml --values /tmp/iscsi_config.yaml
+
+$DIR/tools/wait_for_pods.sh kolla
+
+#helm install kolla/ironic-conductor-daemonset --debug --version $VERSION \
+#    --namespace kolla --name ironic-conductor-daemonset \
+#    --values /tmp/general_config.yaml --values /tmp/iscsi_config.yaml
+
+#helm install kolla/nova-compute-ironic-daemonset --debug --version $VERSION \
+#    --namespace kolla --name nova-compute-ironic-daemonset \
+#    --values /tmp/general_config.yaml --values /tmp/iscsi_config.yaml
 
 $DIR/tools/pull_containers.sh kolla
 $DIR/tools/wait_for_pods.sh kolla
--- tools/setup_gate_iscsi.sh
+++ tools/setup_gate_iscsi.sh
@@ -32,7 +32,8 @@ kubectl taint nodes --all dedicated-
 
 NODE=$(hostname -s)
 kubectl label node $NODE kolla_controller=true kolla_compute=true \
-                         kolla_storage=true kolla_ironic_conductor=true
+                         kolla_storage=true kolla_ironic_conductor=true \
+                         kolla_ironic_compute=true
 
 tests/bin/setup_canal.sh
 
@@ -56,9 +57,10 @@ kollakube res create configmap \
     openvswitch-vswitchd nova-libvirt nova-compute nova-consoleauth \
     nova-novncproxy nova-novncproxy-haproxy neutron-server-haproxy \
     nova-api-haproxy cinder-api cinder-api-haproxy cinder-backup \
-    cinder-scheduler cinder-volume iscsid tgtd keepalived \
-    ironic-api ironic-api-haproxy ironic-conductor ironic-dnsmasq \
-    ironic-inspector ironic-inspector-haproxy ironic-pxe;
+    cinder-scheduler cinder-volume iscsid tgtd keepalived; # \
+#    ironic-api ironic-api-haproxy ironic-conductor ironic-dnsmasq \
+#    ironic-inspector ironic-inspector-haproxy ironic-pxe \
+#    nova-compute-ironic;
 kollakube res create secret nova-libvirt
 
 if [ "x$4" == "xhelm-compute-kit" ]; then
@@ -72,15 +74,23 @@ fi
 #
 # Ironic related commands
 #
-pip install -U python-ironicclient 
-kubectl get pods -n kolla | grep ironic
-kubectl get svc -n kolla | grep ironic
-kubectl get configmaps -n kolla | grep ironic
-kubectl describe svc ironic-api -n kolla
-ironic node-list 
+#pip install -U python-ironicclient 
+#kubectl get pods -n kolla | grep ironic
+#kubectl get svc -n kolla | grep ironic
+#kubectl get configmaps -n kolla | grep ironic
+#kubectl describe svc ironic-api -n kolla
+#ironic node-list 
 #
 # End of Ironic commands
 #
+
+#neutron agent-list
+#nova service-list
+#cinder service-list
+
+#user='root'
+#password=$(python -c 'import yaml; print yaml.safe_load(open("/etc/kolla/passwords.yml"))["database_password"]')
+#kubectl exec mariadb-0 -n kolla -- mysql --user=$user --password=$password -e 'use nova;  select * from compute_nodes;'
 
 sudo pvs >> $WORKSPACE/logs/pvs.txt
 
