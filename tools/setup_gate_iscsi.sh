f8be707e64b3b0a1e334cf39a105b4f7fd7e16d8
Serguei Bezverkhi <sbezverk@cisco.com>
2017-02-16 14:01:58 -0500

    Add nova-compute-ironic microservice chart
    To be able to use ironic in kolla-kubernetes, microservice chart
    for nova-compute-ironic process is required. Also adds
    ironic control plane tests.
    
    Change-Id: I8a9bb1b4192033416809d687ed6d6ac56ecd8e3f

--- tools/setup_gate_iscsi.sh
+++ tools/setup_gate_iscsi.sh
@@ -32,7 +32,8 @@ kubectl taint nodes --all dedicated-
 
 NODE=$(hostname -s)
 kubectl label node $NODE kolla_controller=true kolla_compute=true \
-                         kolla_storage=true kolla_ironic_conductor=true
+                         kolla_storage=true kolla_ironic_conductor=true \
+                         kolla_ironic_compute=true
 
 tests/bin/setup_canal.sh
 
@@ -58,7 +59,8 @@ kollakube res create configmap \
     nova-api-haproxy cinder-api cinder-api-haproxy cinder-backup \
     cinder-scheduler cinder-volume iscsid tgtd keepalived \
     ironic-api ironic-api-haproxy ironic-conductor ironic-dnsmasq \
-    ironic-inspector ironic-inspector-haproxy ironic-pxe;
+    ironic-inspector ironic-inspector-haproxy ironic-pxe \
+    nova-compute-ironic;
 kollakube res create secret nova-libvirt
 
 if [ "x$4" == "xhelm-compute-kit" ]; then
@@ -81,6 +83,14 @@ ironic node-list
 #
 # End of Ironic commands
 #
+
+neutron agent-list
+nova service-list
+cinder service-list
+
+user='root'
+password=$(python -c 'import yaml; print yaml.safe_load(open("/etc/kolla/passwords.yml"))["database_password"]')
+kubectl exec mariadb-0 -n kolla -- mysql --user=$user --password=$password -e 'use nova;  select * from compute_nodes;'
 
 sudo pvs >> $WORKSPACE/logs/pvs.txt
 
