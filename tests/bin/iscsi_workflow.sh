8f11e78ffe5c58958f9a1574c8c9f7469f9c9338
Serguei Bezverkhi <sbezverk@cisco.com>
2017-02-16 14:01:58 -0500

    Add nova-compute-ironic microservice chart
    To be able to use ironic in kolla-kubernetes, microservice chart
    for nova-compute-ironic process is required
    
    Change-Id: I8a9bb1b4192033416809d687ed6d6ac56ecd8e3f

--- tests/bin/iscsi_workflow.sh
+++ tests/bin/iscsi_workflow.sh
@@ -414,15 +414,18 @@ helm install kolla/tgtd-daemonset --version $VERSION \
 #
 helm install kolla/ironic-api-svc --version $VERSION \
     --namespace kolla --name ironic-api-svc \
-    --values /tmp/general_config.yaml --values /tmp/iscsi_config.yaml
+    --values /tmp/general_config.yaml --values /tmp/iscsi_config.yaml \
+    --set "install_type=source,image_tag=master"
 
 helm install kolla/ironic-create-keystone-service-job --version $VERSION \
     --namespace kolla --name ironic-create-keystone-service \
-    --values /tmp/general_config.yaml --values /tmp/iscsi_config.yaml
+    --values /tmp/general_config.yaml --values /tmp/iscsi_config.yaml \
+    --set "install_type=source,image_tag=master"
 
 helm install kolla/ironic-create-keystone-user-job --version $VERSION \
     --namespace kolla --name ironic-create-keystone-user \
-    --values /tmp/general_config.yaml --values /tmp/iscsi_config.yaml
+    --values /tmp/general_config.yaml --values /tmp/iscsi_config.yaml \
+    --set "install_type=source,image_tag=master"
 
 $DIR/tools/wait_for_pods.sh kolla
 
@@ -431,37 +434,49 @@ helm delete --purge ironic-create-keystone-service
 
 helm install kolla/ironic-api-create-db-job --version $VERSION \
     --namespace kolla --name ironic-api-create-db \
-    --values /tmp/general_config.yaml --values /tmp/iscsi_config.yaml
+    --values /tmp/general_config.yaml --values /tmp/iscsi_config.yaml \
+    --set "install_type=source,image_tag=master"
 
 $DIR/tools/wait_for_pods.sh kolla
 
 helm install kolla/ironic-api-manage-db-job --version $VERSION \
     --namespace kolla --name ironic-api-manage-db \
-    --values /tmp/general_config.yaml --values /tmp/iscsi_config.yaml
+    --values /tmp/general_config.yaml --values /tmp/iscsi_config.yaml \
+    --set "install_type=source,image_tag=master"
 
 helm install kolla/ironic-api-create-keystone-endpoint-internal-job --version $VERSION \
     --namespace kolla --name ironic-api-create-keystone-endpoint-internal \
-    --values /tmp/general_config.yaml --values /tmp/iscsi_config.yaml
+    --values /tmp/general_config.yaml --values /tmp/iscsi_config.yaml \
+    --set "install_type=source,image_tag=master"
 
 helm install kolla/ironic-api-create-keystone-endpoint-admin-job --version $VERSION \
     --namespace kolla --name ironic-api-create-keystone-endpoint-admin \
-    --values /tmp/general_config.yaml --values /tmp/iscsi_config.yaml
+    --values /tmp/general_config.yaml --values /tmp/iscsi_config.yaml \
+    --set "install_type=source,image_tag=master"
 
 helm install kolla/ironic-api-create-keystone-endpoint-public-job --version $VERSION \
     --namespace kolla --name ironic-api-create-keystone-endpoint-public \
-    --values /tmp/general_config.yaml --values /tmp/iscsi_config.yaml
+    --values /tmp/general_config.yaml --values /tmp/iscsi_config.yaml \
+    --set "install_type=source,image_tag=master"
 
 $DIR/tools/wait_for_pods.sh kolla
 
-helm install kolla/ironic-api-deployment --debug --version $VERSION \
+helm install kolla/ironic-api-deployment --version $VERSION \
     --namespace kolla --name ironic-api-deployment \
-    --values /tmp/general_config.yaml --values /tmp/iscsi_config.yaml
+    --values /tmp/general_config.yaml --values /tmp/iscsi_config.yaml \
+    --set "install_type=source,image_tag=master"
 
 $DIR/tools/wait_for_pods.sh kolla
 
 helm install kolla/ironic-conductor-daemonset --debug --version $VERSION \
     --namespace kolla --name ironic-conductor-daemonset \
-    --values /tmp/general_config.yaml --values /tmp/iscsi_config.yaml
+    --values /tmp/general_config.yaml --values /tmp/iscsi_config.yaml \
+    --set "install_type=source,image_tag=master"
+
+helm install kolla/nova-compute-ironic-daemonset --debug --version $VERSION \
+    --namespace kolla --name nova-compute-ironic-daemonset \
+    --values /tmp/general_config.yaml --values /tmp/iscsi_config.yaml \
+    --set "install_type=source,image_tag=master"
 
 $DIR/tools/pull_containers.sh kolla
 $DIR/tools/wait_for_pods.sh kolla
