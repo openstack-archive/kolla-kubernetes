655fe7678bcbf87d1c10a2514d5bd6b8fa4e2e65
Serguei Bezverkhi <sbezverk@cisco.com>
2017-02-23 11:22:04 -0500

    Configuring ironic gates (basic tests)
    Configuring ironic gates (basic tests)
    
    Change-Id: I30339a1e2a8b95ad21fc2c5400eb9f765b91c783

---
+++ tests/bin/iscsi_ironic_workflow.sh
@@ -0,0 +1,25 @@
+#!/bin/bash -xe
+#
+# Passed parameters $1 - Config, $2 - Distro, $3 - Branch
+#
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/../.." && pwd )"
+VERSION=0.6.0-1
+IP=172.18.0.1
+tunnel_interface=docker0
+base_distro="$2"
+branch=$3
+config=$1
+
+. "$DIR/tests/bin/deploy_iscsi_common.sh"
+. "$DIR/tests/bin/deploy_ironic.sh"
+
+#
+# Deploying common iscsi components
+#
+deploy_iscsi_common  $IP $base_distro $tunnel_interface $branch $config
+
+#
+# Deploying ironic
+#
+deploy_ironic  $IP $base_distro $tunnel_interface $branch $config
