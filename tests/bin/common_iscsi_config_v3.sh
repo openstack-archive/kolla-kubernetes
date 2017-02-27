6e1c93f993685a3f245bf52b89b26b513bf78edf
Serguei Bezverkhi <sbezverk@cisco.com>
2017-02-27 09:49:25 -0500

    WIP Adding ironic inspector microservices
    Adding ironic inspector microservices
    
    Change-Id: I015a839a6305953ea0d184ee8e1ef04d2427a101
    Partially-Implements: blueprint more-microservices

--- tests/bin/common_iscsi_config_v3.sh
+++ tests/bin/common_iscsi_config_v3.sh
@@ -21,6 +21,11 @@ function common_iscsi_config {
     echo "    ironic:"
     echo "      all:"
     echo "        image_tag: 3.0.3-beta.1"
+    echo "      dnsmasq:
+    echo "        all:
+    echo "          initramf_url: "http://tarballs.openstack.org/ironic-python-agent/tinyipa/files/tinyipa-stable-newton.gz"
+    echo "          kernel_url: "http://tarballs.openstack.org/ironic-python-agent/tinyipa/files/tinyipa-stable-newton.vmlinuz"
+    echo "          ironic_dhcp_range: net2,172.22.0.10,172.22.0.20,255.255.255.0,24h"
     echo "    horizon:"
     echo "      all:"
     echo "        image_tag: 3.0.3-beta.1"
