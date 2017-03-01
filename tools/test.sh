18cd9f245b8663717288c69fd01575ac23aea4fa
Steven Dake <stdake@cisco.com>
2017-03-01 10:38:54 -0700

    Fix problem with kollakube res create configmap
    After the repository split of kolla into kolla and kolla-ansible,
    kollakube was not updated to reflect that change.  As a result,
    kollakube needs a small tweak.
    
    Co-Authored-By: zhubingbing <zhu.bingbing@99cloud.net>
    
    Change-Id: Ib5156c0a22cce22dbd819afd2bb29cf22e72b109

--- tools/test.sh
+++ tools/test.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/" && pwd )"
-[ ! -d ../kolla ] && pushd .. && git clone https://github.com/openstack/kolla-ansible && mv kolla-ansible kolla && popd
-grep api_interface_address ../kolla/etc/kolla/globals.yml || echo api_interface_address: "0.0.0.0" >> ../kolla/etc/kolla/globals.yml
-grep tunnel_interface_address ../kolla/etc/kolla/globals.yml || echo tunnel_interface_address: "0.0.0.0" >> ../kolla/etc/kolla/globals.yml
-grep orchestration_engine ../kolla/etc/kolla/globals.yml || echo orchestration_engine: KUBERNETES >> ../kolla/etc/kolla/globals.yml
+[ ! -d ../kolla-ansible ] && pushd .. && git clone https://github.com/openstack/kolla-ansible  && popd
+grep api_interface_address ../kolla-ansible/etc/kolla/globals.yml || echo api_interface_address: "0.0.0.0" >> ../kolla-ansible/etc/kolla/globals.yml
+grep tunnel_interface_address ../kolla-ansible/etc/kolla/globals.yml || echo tunnel_interface_address: "0.0.0.0" >> ../kolla-ansible/etc/kolla/globals.yml
+grep orchestration_engine ../kolla-ansible/etc/kolla/globals.yml || echo orchestration_engine: KUBERNETES >> ../kolla-ansible/etc/kolla/globals.yml
 #sudo yum install -y golang-bin || sudo apt-get install -y golang
 #tools/build_helm_templates.sh
 set -x
