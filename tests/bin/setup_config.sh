bbba4f1bddcbb5fab4afa5daf27d84c5cb047b27
Steven Dake <stdake@cisco.com>
2017-03-01 10:38:54 -0700

    Fix problem with kollakube res create configmap
    After the repository split of kolla into kolla and kolla-ansible,
    kollakube was not updated to reflect that change.As a result,
    kollakube needs a small tweak.
    
    Co-Authored-By: zhubingbing <zhu.bingbing@99cloud.net>
    
    Change-Id: Ib5156c0a22cce22dbd819afd2bb29cf22e72b109

--- tests/bin/setup_config.sh
+++ tests/bin/setup_config.sh
@@ -5,64 +5,59 @@ NODE=$(hostname -s)
 TYPE="$2"
 BRANCH="$3"
 
-echo "kolla_base_distro: $1" >> kolla-ansible/etc/kolla/globals.yml
-cat tests/conf/ceph-all-in-one/kolla_config >> kolla-ansible/etc/kolla/globals.yml
+echo "kolla_base_distro: $1" | sudo tee -a /etc/kolla/globals.yml
+
 IP=172.18.0.1
-sed -i "s/^\(kolla_external_vip_address:\).*/\1 '$IP'/" \
-    kolla-ansible/etc/kolla/globals.yml
-sed -i "s/^\(kolla_kubernetes_external_vip:\).*/\1 '$IP'/" \
-    etc/kolla-kubernetes/kolla-kubernetes.yml
 
-cat tests/conf/ceph-all-in-one/kolla_kubernetes_config \
-    >> etc/kolla-kubernetes/kolla-kubernetes.yml
+sudo -E sed -i "s/^\(kolla_external_vip_address:\).*/\1 '$IP'/" /etc/kolla/globals.yml
+sudo -E sed -i "s/^\(kolla_kubernetes_external_vip:\).*/\1 '$IP'/" /etc/kolla-kubernetes/kolla-kubernetes.yml
+sudo -E sed -i "s/initial_mon:.*/initial_mon: $NODE/" /etc/kolla-kubernetes/kolla-kubernetes.yml
 
-sed -i "s/initial_mon:.*/initial_mon: $NODE/" \
-    etc/kolla-kubernetes/kolla-kubernetes.yml
+cat tests/conf/ceph-all-in-one/kolla_config | sudo tee -a  /etc/kolla/globals.yml
+cat tests/conf/ceph-all-in-one/kolla_kubernetes_config | sudo tee -a /etc/kolla-kubernetes/kolla-kubernetes.yml
 
 if [ "x$TYPE" == "xceph-multi" ]; then
     interface=$(netstat -ie | grep -B1 \
         $(cat /etc/nodepool/primary_node_private) \
         | head -n 1 | awk -F: '{print $1}')
-    echo "tunnel_interface: $interface" >> kolla-ansible/etc/kolla/globals.yml
-    echo "storage_interface: $interface" >> \
-        etc/kolla-kubernetes/kolla-kubernetes.yml
-    sed -i "s/172.17.0.1/$(cat /etc/nodepool/primary_node_private)/" \
-        etc/kolla-kubernetes/kolla-kubernetes.yml
+    echo "tunnel_interface: $interface" | sudo tee -a /etc/kolla/globals.yml
+    echo "storage_interface: $interface" | sudo tee -a /etc/kolla-kubernetes/kolla-kubernetes.yml
+    sudo sed -i "s/172.17.0.1/$(cat /etc/nodepool/primary_node_private)/" /etc/kolla-kubernetes/kolla-kubernetes.yml
 fi
 
 if [ "x$BRANCH" == "x2" -o "x$BRANCH" == "x3" ]; then
-    echo 'enable_placement: "no"' >> kolla-ansible/etc/kolla/globals.yml
+    echo 'enable_placement: "no"' | sudo tee -a /etc/kolla/globals.yml
 fi
 
-kolla-ansible/tools/generate_passwords.py
-kolla-ansible/tools/kolla-ansible genconfig
+sudo kolla-genpwd
+sudo kolla-ansible genconfig
 
 # Testing ansible-in-k8s approach
-rm -rf /etc/kolla/neutron*
-rm -rf /etc/kolla/keystone*
-rm -rf /etc/kolla/glance*
-rm -rf /etc/kolla/horizon*
-rm -rf /etc/kolla/nova*
-rm -rf /etc/kolla/memcached*
-rm -rf /etc/kolla/cinder*
+sudo rm -rf /etc/kolla/neutron*
+sudo rm -rf /etc/kolla/keystone*
+sudo rm -rf /etc/kolla/glance*
+sudo rm -rf /etc/kolla/horizon*
+sudo rm -rf /etc/kolla/nova*
+sudo rm -rf /etc/kolla/memcached*
+sudo rm -rf /etc/kolla/cinder*
 
-ansible-playbook -e ansible_python_interpreter=/usr/bin/python -e @/etc/kolla/globals.yml -e @/etc/kolla/passwords.yml -e CONFIG_DIR=/etc/kolla ansible/site.yml
+sudo -E ansible-playbook -e ansible_python_interpreter=/usr/bin/python -e @/etc/kolla/globals.yml -e @/etc/kolla/passwords.yml -e CONFIG_DIR=/etc/kolla ansible/site.yml
 ls -la /etc/kolla
 
-crudini --set /etc/kolla/nova-compute/nova.conf libvirt virt_type qemu
-crudini --set /etc/kolla/nova-compute/nova.conf libvirt cpu_mode none
-crudini --set /etc/kolla/nova-compute/nova.conf libvirt rbd_user nova
+sudo crudini --set /etc/kolla/nova-compute/nova.conf libvirt virt_type qemu
+sudo crudini --set /etc/kolla/nova-compute/nova.conf libvirt cpu_mode none
+sudo crudini --set /etc/kolla/nova-compute/nova.conf libvirt rbd_user nova
 UUID=$(awk '{if($1 == "rbd_secret_uuid:"){print $2}}' /etc/kolla/passwords.yml)
-crudini --set /etc/kolla/nova-compute/nova.conf libvirt rbd_secret_uuid $UUID
+sudo -E crudini --set /etc/kolla/nova-compute/nova.conf libvirt rbd_secret_uuid $UUID
 
 # Keystone does not seem to invalidate its cache on entry point addition.
-crudini --set /etc/kolla/keystone/keystone.conf cache enabled False
+sudo crudini --set /etc/kolla/keystone/keystone.conf cache enabled False
 
-sed -i 's/log_outputs = "3:/log_outputs = "1:/' /etc/kolla/nova-libvirt/libvirtd.conf
-sed -i 's/log_level = 3/log_level = 1/' /etc/kolla/nova-libvirt/libvirtd.conf
+sudo sed -i 's/log_outputs = "3:/log_outputs = "1:/' /etc/kolla/nova-libvirt/libvirtd.conf
+sudo sed -i 's/log_level = 3/log_level = 1/' /etc/kolla/nova-libvirt/libvirtd.conf
 
-sed -i \
+sudo sed -i \
     '/\[global\]/a osd pool default size = 1\nosd pool default min size = 1\nosd crush chooseleaf type = 0\ndebug default = 5\n'\
     /etc/kolla/ceph*/ceph.conf
 
-./tools/fix-mitaka-config.py
+sudo -E ./tools/fix-mitaka-config.py
