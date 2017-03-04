8f72cc1fd6a7d74019a4c91642e54e694e56978d
Serguei Bezverkhi <sbezverk@cisco.com>
2017-03-03 23:35:30 -0500

    WIP Adding virtualbmc to the gate
    This PS adds required packages to be able to power on/off VM by using
    virtualbmc and virsh client interface.
    
    Change-Id: Ib1ad8d38da1b7654b21c76581f9e5ab575c440a6

---
+++ tests/bin/iscsi_ironic_vm_workflow.sh
@@ -0,0 +1,61 @@
+#!/bin/bash -xe
+#
+# Passed parameters $1 - Config, $2 - Distro, $3 - Branch
+#
+
+function wait_for_ironic_node {
+    set +x
+    count=0
+    while true; do
+        val=$(openstack baremetal node list -c "Provisioning State" -f value)
+        node_id=$(openstack baremetal node list -c "UUID" -f value)
+        [ $val == "available" ] && break
+        [ $val == "error" ] && openstack baremetal node show $node_id && exit -1
+        sleep 1;
+        count=$((count+1))
+        [ $count -gt 30 ] && openstack baremetal node show $node_id && exit -1
+    done
+    set -x
+}
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/../.." && pwd )"
+VERSION=0.6.0-1
+IP=172.18.0.1
+tunnel_interface=docker0
+base_distro="$2"
+branch="$3"
+config="$1"
+
+. "$DIR/tests/bin/deploy_iscsi_common.sh"
+. "$DIR/tests/bin/deploy_ironic.sh"
+
+function common_iscsi {
+   deploy_iscsi_common  $IP $base_distro $tunnel_interface $branch $config
+}
+
+function ironic {
+   deploy_ironic  $IP $base_distro $tunnel_interface $branch $config
+}
+
+#
+# Deploying common iscsi components
+#
+common_iscsi
+
+DISTRO=$2
+CONFIG=$1
+if [ "x$DISTRO" == "xubuntu" ]; then
+exit 0
+else
+  sudo yum install -y libvirt qemu-kvm ipmitool libvirt-devel || true
+  sudo systemctl start libvirtd
+  sudo pip install libvirt-python
+fi
+
+git clone https://github.com/openstack/virtualbmc
+cd virtualbmc/
+sudo pip install -U .
+#ipmitool -I lanplus -U admin -P password -H 127.0.0.1 power status
+sudo virsh list --all
+vbmc list
+exit 0
