2df4496149593fc955d8815a10f33847ac6d7d12
Serguei Bezverkhi <sbezverk@cisco.com>
2017-03-11 18:42:17 -0500

    WIP Replace unconditional wait for compute kit
    Currently After the compute kit charts are deployed, the gate task
    unconditionally wait for 10 minutes before running basic tests. This
    patch replaces unconditional wait timer for a function which verifies
    services individually and if the services are up, exit with 0.
    
    Change-Id: Id1bc147b824fc73d3652db909846adeb9c89cce2
    Closes-Bug: #1664322

--- tests/bin/deploy_compute_kit.sh
+++ tests/bin/deploy_compute_kit.sh
@@ -10,6 +10,74 @@ IP=172.18.0.1
 tunnel_interface=docker0
 base_distro="$2"
 
+function check_for_nova {
+    for service in nova-scheduler nova-conductor nova-compute;
+        do
+           status=$(nova service-list | grep $service | awk '{print $12}')
+           if [ "x$status" != "xup" ]; then
+              return 1
+           fi
+        done
+    return 0
+}
+
+function check_for_neutron {
+    for service in neutron-l3-agent neutron-metadata-agent neutron-dhcp-agent \
+                   neutron-openvswitch-agent;
+        do
+           status=$(neutron agent-list | grep $service | awk '{print $12}')
+           if [ "x$status" != "x:-)" ]; then
+              return 1
+           fi
+        done
+    return 0
+}
+
+function check_for_cindrer {
+    for service in cinder-scheduler cinder-volume;
+        do
+           status=$(cinder service-list | grep $service | awk '{print $10}')
+           if [ "x$status" != "xup" ]; then
+              return 1
+           fi
+        done
+    return 0
+}
+
+function wait_for_openstack {
+    set +ex
+    count=0
+    while true; do
+        [ $count -gt 600 ] && echo Wait for openstack services failed... \
+                           && return -1
+        echo "Check for nova"
+        check_for_nova
+        retcode=$?
+        if [ $retcode -eq 1 ]; then
+           sleep 1
+           count=$((count+1))
+           continue
+        fi
+        echo "check for neutron"
+        check_for_neutron
+        retcode=$?
+        if [ $retcode -eq 1 ]; then
+           sleep 1
+           count=$((count+1))
+           continue
+        fi
+        echo "check for cinder"
+        check_for_cinder
+        retcode=$?
+        if [ $retcode -eq 1 ]; then
+           sleep 1
+           count=$((count+1))
+           continue
+        fi
+    done
+    set -ex
+}
+
 function general_config {
     common_workflow_config $IP $base_distro $tunnel_interface
 }
@@ -33,6 +101,17 @@ $DIR/tools/wait_for_pods.sh kolla
 
 # It looks like after containers are up OpenStack services needs extra time to
 # come up, giving extra 10 minutes to settle.
+$DIR/tools/build_local_admin_keystonerc.sh
+. ~/keystonerc_admin
+pip list
+nova service-list
+neutron agent-list
+cinder service-list
+
 sleep 600
 
-$DIR/tools/build_local_admin_keystonerc.sh
+nova service-list
+neutron agent-list
+agent_id=$(neutron agent-list | grep neutron-l3-agent | awk '{print $2}')
+neutron agent-show $agent_id
+cinder service-list
