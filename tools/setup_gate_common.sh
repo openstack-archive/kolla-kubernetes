91a66ff31666dce6078966bbd3a090cb073f0461
Serguei Bezverkhi <sbezverk@cisco.com>
2017-03-24 16:08:09 -0400

    Fixing ironic deployment failure on Ubuntu
    Due to some particularities of libvirtd on Ubuntu, it runs with nova user
    privileges. It breaks virsh commands runing from the host.
    
    Change-Id: Icb1db1000e02c2851f92ed76258e07a78863f897

--- tools/setup_gate_common.sh
+++ tools/setup_gate_common.sh
@@ -112,6 +112,16 @@ sudo bash -c 'cat << EOF >> /etc/kolla/config/ironic.conf
 tftp_server = undefined
 pxe_append_params = nofb nomodeset vga=normal console=tty0 console=ttyS0,115200n8
 EOF'
+sudo mkdir -p /etc/kolla/config/nova-libvirt/
+sudo bash -c 'cat << EOF >> /etc/kolla/config/nova-libvirt/libvirtd.conf
+unix_sock_ro_perms = "0777"
+unix_sock_rw_perms = "0777"
+auth_unix_ro = "none"
+auth_unix_rw = "none"
+EOF'
+sudo bash -c 'cat << EOF >> /etc/kolla/config/nova-libvirt/qemu.conf
+group = "root"
+EOF'
 fi
 }
 
