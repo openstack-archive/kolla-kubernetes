bb07a07a77170ced7e95fb3b64512ebe9c29f7cc
Kevin Fox <kevin@efox.cc>
2017-03-24 13:49:57 -0700

    Helm Version File
    This moves all Helm version information to one file so it can
    be more easily updated.
    
    Change-Id: I392209ec427650dee1befaf49522a8d8691aa9d8

---
+++ tools/helm_versions.sh
@@ -0,0 +1,4 @@
+HELM_VERSION=2.2.2
+HELM_TEMPLATE_URL=https://github.com/technosophos/helm-template/releases/download/2.2.2%2B1/helm-template-linux-2.2.2.1.tgz
+
+HELM_URL="http://storage.googleapis.com/kubernetes-helm/helm-v$HELM_VERSION-linux-amd64.tar.gz"
