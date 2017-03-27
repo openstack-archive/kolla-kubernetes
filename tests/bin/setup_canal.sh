1ce93944e400305595908ac1191aa2f59e4200aa
Kevin Fox <kevin@efox.cc>
2017-03-27 12:09:01 -0700

    WIP: Unpin Canal
    This PS unpins Canal
    
    Change-Id: I52701aa0038a69b01742f12b950cdc038478aa5b

--- tests/bin/setup_canal.sh
+++ tests/bin/setup_canal.sh
@@ -2,14 +2,14 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/../.." && pwd )"
 
-url="https://raw.githubusercontent.com/tigera/canal/master"
-url="https://raw.githubusercontent.com/projectcalico/canal/7deb07cda04147ab49115f437151705c747d0374"
+url="https://raw.githubusercontent.com/projectcalico/canal/master"
 url="$url/k8s-install/kubeadm/canal.yaml"
 
 curl "$url" -o /tmp/canal.yaml
 
 sed -i "s@192.168.0.0/16@172.16.130.0/22@" /tmp/canal.yaml
 sed -i "s@10.96.232.136@172.16.128.100@" /tmp/canal.yaml
+sed -i '/CALICO_DISABLE_FILE_LOGGING/i\            - name: FELIX_DEFAULTENDPOINTTOHOSTACTION\n              value: "ACCEPT"' /tmp/canal.yaml
 
 kubectl create -f /tmp/canal.yaml
 
