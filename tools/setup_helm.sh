640941201547f72a54b249e9d4d14c015e916eab
Kevin Fox <kevin@efox.cc>
2017-03-27 13:38:29 -0700

    WIP: Helm Native Values.
    This PS starts converting away from generating microservices
    values.yaml to building it using the new helm native parent values
    feature.
    
    Change-Id: Ieac62538662fc267bb42e2c795a05bdd8d8e3120

--- tools/setup_helm.sh
+++ tools/setup_helm.sh
@@ -5,6 +5,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/" && pwd )"
 . tools/helm_versions.sh 
 
 curl "$HELM_URL" | sudo tar --strip-components 1 -C /usr/bin linux-amd64/helm -zxf -
+#curl https://raw.githubusercontent.com/jascott1/bins/master/helm/_dist/linux-amd64/helm | sudo dd of=/usr/bin/helm
 if [ ! -e ~/.kube/config ]; then
     mkdir -p ~/.kube
     sudo cat /etc/kubernetes/kubelet.conf > ~/.kube/config
