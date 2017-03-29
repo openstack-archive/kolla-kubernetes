d03c1754a8d40fc15296a27367b7da27df953c90
Steven Dake <stdake@cisco.com>
2017-03-29 13:44:26 -0700

    Pin gate to working versions of kubernetes
    Pin Kubernetes to 1.5.4 on CentOS 7
    Pin Kubernetes to 1.5.6 on Ubuntu Xenial
    
    Change-Id: Ia2825c21fbb14345be08c23adab6bd7471a98c35

--- tests/bin/fix_gate_iptables.sh
+++ tests/bin/fix_gate_iptables.sh
@@ -4,7 +4,7 @@ l=$(sudo iptables -L INPUT --line-numbers | grep openstack-INPUT | \
     awk '{print $1}')
 sudo iptables -D INPUT $l
 
-ip a | sed '/^[^1-9]/d;' | awk '{print $2}' | sed 's/://' | \
+/usr/sbin/ip a | sed '/^[^1-9]/d;' | awk '{print $2}' | sed 's/://' | \
     grep -v '^lo$' | while read line; do
     sudo iptables -I INPUT 1 -i $line -j openstack-INPUT
 done
