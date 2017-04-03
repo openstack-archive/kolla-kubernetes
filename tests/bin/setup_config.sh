6543ea7bbe99bca9475b32290ab7b8781021bb01
Michal (inc0) Jastrzebski <inc007@gmail.com>
2017-04-03 17:25:37 +0000

    Move nova confs to k8s
    Another step...
    
    Change-Id: I4101b7b198a6df0007586029cd3ee9030cebd131

--- tests/bin/setup_config.sh
+++ tests/bin/setup_config.sh
@@ -39,6 +39,7 @@ rm -rf /etc/kolla/neutron*
 rm -rf /etc/kolla/keystone*
 rm -rf /etc/kolla/glance*
 rm -rf /etc/kolla/horizon*
+rm -rf /etc/kolla/nova*
 
 ansible-playbook -e ansible_python_interpreter=/usr/bin/python -e @/etc/kolla/globals.yml -e @/etc/kolla/passwords.yml -e CONFIG_DIR=/etc/kolla ansible/site.yml
 ls -la /etc/kolla
