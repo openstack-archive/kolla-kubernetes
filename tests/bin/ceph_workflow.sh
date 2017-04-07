f9549e5315159612631c7cf76a7fa8bade1d5725
Kevin Fox <kevin@efox.cc>
2017-04-07 11:21:21 -0700

    WIP: v4 gates.
    This PS adds support for kolla 4.x containers to the gate.
    
    Change-Id: I98917ebe58b010aff39d7d9539cb2a7cdbd460ac

--- tests/bin/ceph_workflow.sh
+++ tests/bin/ceph_workflow.sh
@@ -513,7 +513,24 @@ helm install kolla/nova-libvirt-daemonset --version $VERSION \
 
 helm install kolla/nova-compute-daemonset --version $VERSION \
     --namespace kolla --name nova-compute-daemonset \
+    --values /tmp/general_config.yaml --values /tmp/ceph_config.yaml \
+    --set image_tag=3.0.3
+#FIXME for testing...
+
+$DIR/tools/pull_containers.sh kolla
+$DIR/tools/wait_for_pods.sh kolla
+
+if [ "x$branch" != "x2" -a "x$branch" != "x3" ]; then
+helm install kolla/nova-cell0-create-db-job --debug --version $VERSION \
+    --namespace kolla --name nova-cell0-create-db-job \
     --values /tmp/general_config.yaml --values /tmp/ceph_config.yaml
+
+$DIR/tools/wait_for_pods.sh kolla
+
+helm install kolla/nova-api-create-simple-cell-job --debug --version $VERSION \
+    --namespace kolla --name nova-api-create-simple-cell-job \
+    --values /tmp/general_config.yaml --values /tmp/ceph_config.yaml
+fi
 
 $DIR/tools/pull_containers.sh kolla
 $DIR/tools/wait_for_pods.sh kolla
