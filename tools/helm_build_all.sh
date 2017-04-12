f9caec5b610e09c48614c5d7af94040763de314d
Kevin Fox <kevin@efox.cc>
2017-04-12 15:33:38 -0700

    Compress compute kit
    This PS adds a quick workaround for a max release size limitation.
    
    Change-Id: If610ca71fe0a743a5d9abfb7f54c71285fd5a93b

--- tools/helm_build_all.sh
+++ tools/helm_build_all.sh
@@ -20,5 +20,16 @@ $DIR/helm_build_services.py "$REPODIR"
 $DIR/helm_prebuild_compute_kits.py
 $DIR/helm_build_compute_kits.py "$REPODIR"
 
+#FIXME this really belongs in helm_build_*.
+# This deduplicates templates shrinking the size of
+# the compute-kit package. Otherwise, it blows out the
+# helm release k8s 1m configmap limit.
+D=/tmp/computekit-$$/
+mkdir -p $D
+tar -C $D --exclude "charts/*/kolla-common/templates/*" -xf "$REPODIR/compute-kit-0.6.0-1.tgz"
+pushd "$REPODIR"
+helm package "$D/compute-kit"
+popd
+
 helm repo index "$REPODIR"
 helm repo update
