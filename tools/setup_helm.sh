ba9c6bfc4babb24fd62d08b3caf06465bfede488
Kevin Fox <kevin@efox.cc>
2017-04-14 13:36:44 -0700

    WIP: Test out flannel helm package.
    DO NOT MERGE. DO NOT REVIEW...
    
    Just messing around.
    
    Change-Id: I9a20d7ac2575505618f66ea792369b389ecab273

--- tools/setup_helm.sh
+++ tools/setup_helm.sh
@@ -4,13 +4,12 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/" && pwd )"
 
 . tools/helm_versions.sh 
 
-curl "$HELM_URL" | sudo tar --strip-components 1 -C /usr/bin linux-amd64/helm -zxf -
+#curl "$HELM_URL" | sudo tar --strip-components 1 -C /usr/bin linux-amd64/helm -zxf -
+sudo curl -o /usr/bin/helm https://raw.githubusercontent.com/jascott1/bins/master/helm/nethost/_dist/linux-amd64/helm
+sudo chmod +x /usr/bin/helm
 if [ ! -e ~/.kube/config ]; then
     mkdir -p ~/.kube
     sudo cat /etc/kubernetes/kubelet.conf > ~/.kube/config
 fi
 
-$DIR/pull_containers.sh kube-system
-$DIR/wait_for_pods.sh kube-system
-
-helm init
+helm init --host-net
