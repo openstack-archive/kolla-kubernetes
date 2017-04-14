f5f373416c4e9ab955fda2de96b65de5ee9c8d9e
Kevin Fox <kevin@efox.cc>
2017-04-14 13:36:44 -0700

    WIP: Test out flannel helm package.
    DO NOT MERGE. DO NOT REVIEW...
    
    Just messing around.
    
    Change-Id: I9a20d7ac2575505618f66ea792369b389ecab273

--- tests/bin/setup_canal.sh
+++ tests/bin/setup_canal.sh
@@ -2,15 +2,22 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/../.." && pwd )"
 
-url="https://raw.githubusercontent.com/projectcalico/canal/master"
-url="$url/k8s-install/kubeadm/1.6/canal.yaml"
+#url="https://raw.githubusercontent.com/projectcalico/canal/master"
+#url="$url/k8s-install/kubeadm/1.6/canal.yaml"
 
-curl "$url" -o /tmp/canal.yaml
+#curl "$url" -o /tmp/canal.yaml
 
-sed -i "s@192.168.0.0/16@172.16.132.0/22@" /tmp/canal.yaml
-sed -i "s@10.96.232.136@172.16.128.100@" /tmp/canal.yaml
+#sed -i "s@192.168.0.0/16@172.16.132.0/22@" /tmp/canal.yaml
+#sed -i "s@10.96.232.136@172.16.128.100@" /tmp/canal.yaml
 
-kubectl create -f /tmp/canal.yaml
+#kubectl create -f /tmp/canal.yaml
+
+git clone https://github.com/kfox1111/charts
+pushd charts
+git fetch origin flannel:flannel
+git checkout flannel
+helm install --name flannel stable/flannel --set 'subnet=172.16.132.0/22'
+popd
 
 $DIR/tools/pull_containers.sh kube-system
 $DIR/tools/wait_for_pods.sh kube-system 240
--- tools/setup_helm.sh
+++ tools/setup_helm.sh
@@ -4,7 +4,9 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/" && pwd )"
 
 . tools/helm_versions.sh 
 
-curl "$HELM_URL" | sudo tar --strip-components 1 -C /usr/bin linux-amd64/helm -zxf -
+#curl "$HELM_URL" | sudo tar --strip-components 1 -C /usr/bin linux-amd64/helm -zxf -
+sudo curl -o /usr/bin/helm https://raw.githubusercontent.com/jascott1/bins/master/helm/nethost/_dist/linux-amd64/helm
+sudo chmod +x /usr/bin/helm
 if [ ! -e ~/.kube/config ]; then
     mkdir -p ~/.kube
     sudo cat /etc/kubernetes/kubelet.conf > ~/.kube/config
@@ -13,4 +15,4 @@ fi
 $DIR/pull_containers.sh kube-system
 $DIR/wait_for_pods.sh kube-system
 
-helm init
+helm init --host-net
