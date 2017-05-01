9919fbc6ee7f851e7afe9896696ace62b5888fc9
Serguei Bezverkhi <sbezverk@cisco.com>
2017-05-01 18:26:31 -0400

    Fix for new canal.yaml location
    Fix for new canal.yaml location
    
    Change-Id: Ib4e6ac01501373eb04c388b1290f77f9563d130d

--- tests/bin/setup_canal.sh
+++ tests/bin/setup_canal.sh
@@ -3,12 +3,35 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/../.." && pwd )"
 
 url="https://raw.githubusercontent.com/projectcalico/canal/master"
-url="$url/k8s-install/kubeadm/1.6/canal.yaml"
+url="$url/k8s-install/1.6/canal.yaml"
 
 curl "$url" -o /tmp/canal.yaml
 
-sed -i "s@192.168.0.0/16@172.16.132.0/22@" /tmp/canal.yaml
-sed -i "s@10.96.232.136@172.16.128.100@" /tmp/canal.yaml
+url="https://raw.githubusercontent.com/projectcalico/canal/master"
+url="$url/k8s-install/1.6/rbac.yaml"
+
+curl "$url" -o /tmp/rbac.yaml
+
+kubectl create -f /tmp/rbac.yaml
+
+sudo bash -c 'cat /etc/kubernetes/manifests/kube-controller-manager.yaml'
+
+cluster_cidr=$(sudo grep cluster-cidr /etc/kubernetes/manifests/kube-controller-manager.yaml || true)
+cluster_cidr=${cluster_cidr##*=}
+network_conf=$(grep net-conf.json /tmp/canal.yaml || true)
+if [  "x$network_conf" == "x"]; then
+   sed -i '/masquerade:/a\
+  net-conf.json: |\
+    {\
+      "Network": "'$cluster_cidr'",\
+      "Backend": {\
+        "Type": "vxlan"\
+      }\
+    }' /tmp/canal.yaml
+else
+   sed -i 's@"Network":.*"@"Network": "'$cluster_cidr'"@' /tmp/canal.yaml
+fi
+cat /tmp/canal.yaml
 
 kubectl create -f /tmp/canal.yaml
 
