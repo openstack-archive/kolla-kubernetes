4145621bb17a2420ca283210b0bfd7eac0f3bd39
Serguei Bezverkhi <sbezverk@cisco.com>
2017-05-01 18:26:31 -0400

    Fix for new canal.yaml location
    Fix for new canal.yaml location
    
    Change-Id: Ib4e6ac01501373eb04c388b1290f77f9563d130d

--- tests/bin/fix_gate_iptables.sh
+++ tests/bin/fix_gate_iptables.sh
@@ -4,6 +4,12 @@ l=$(sudo iptables -L INPUT --line-numbers | grep openstack-INPUT | \
     awk '{print $1}')
 sudo iptables -D INPUT $l
 
+#
+# Temporary, just trying to understand why centos does not find this command
+#
+sudo ls -al /usr/sbin/ip || true
+sudo find / -name ip | grep -v var
+
 ip a | sed '/^[^1-9]/d;' | awk '{print $2}' | sed 's/://' | \
     grep -v '^lo$' | while read line; do
     sudo iptables -I INPUT 1 -i $line -j openstack-INPUT
