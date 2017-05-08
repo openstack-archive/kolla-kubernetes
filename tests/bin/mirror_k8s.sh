e1ad83ca09727512bd2a54bc5fe5a37be3a3570e
Kevin Fox <kevin@efox.cc>
2017-05-08 06:52:43 -0700

    WIP: Cache k8s
    This PS caches the latest working Kubernetes software to insulate
    the gate from sudden changes in the released binaries.
    
    Change-Id: I2bd79073c66da24e57bf57a4e49ae2281a90b4df

---
+++ tests/bin/mirror_k8s.sh
@@ -0,0 +1,26 @@
+LOGS="$1"
+DISTRO="$2"
+TYPE="$3"
+CONFIG="$4"
+BRANCH="$5"
+PIPELINE="$6"
+
+#FIXME move this to periodic only once done testing.
+if [ "x$DISTRO" == "xcentos" ]; then
+    sudo yum install -y createrepo
+    mkdir -p $WORKSPACE/UPLOAD_REPOS/kubernetes
+    sudo cp -a /var/cache/yum/kubernetes/packages/*.rpm $WORKSPACE/UPLOAD_REPOS/kubernetes
+    sudo chown $USER $WORKSPACE/UPLOAD_REPOS/kubernetes
+    chmod 644 $WORKSPACE/UPLOAD_REPOS/kubernetes/
+    createrepo $WORKSPACE/UPLOAD_REPOS/kubernetes
+    tar -C $WORKSPACE/UPLOAD_REPOS/ -jcvf ../kubernetes.tar.bz2 kubernetes
+    mv ../kubernetes.tar.bz2 $WORKSPACE/UPLOAD_REPOS/
+    rm -rf WORKSPACE/UPLOAD_REPOS/kubernetes
+    ls -l $WORKSPACE/UPLOAD_REPOS/
+#FIXME
+    mv $WORKSPACE/UPLOAD_REPOS/kubernetes.tar.bz2 $WORKSPACE/logs/
+fi
+
+if [ "x$PIPELINE" == "xperiodic" -a "x$DISTRO" == "xcentos" ]; then
+    echo
+fi
