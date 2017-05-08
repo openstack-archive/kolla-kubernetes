497159ce75d760631a2e10ca971540523d8ca989
Kevin Fox <kevin@efox.cc>
2017-05-08 07:13:50 -0700

    WIP: Start of upgrade tests.
    FIXME, we should used released n-1 packages and only build the N packages.
    
    Change-Id: I1bc0bbfe38674ec8f281d2b7e1bf2d1ddc76e4ff

---
+++ tests/bin/upgrade_test_ceph.sh
@@ -0,0 +1,56 @@
+#!/bin/bash -xe
+
+echo Upgrading test ceph.
+
+VERSION=0.7.0-1
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/../.." && pwd )"
+IP="$3"
+base_distro="$2"
+gate_job="$1"
+tunnel_interface="$4"
+branch="$5"
+
+. "$DIR/tests/bin/common_workflow_config.sh"
+
+function general_config {
+    common_workflow_config $IP $base_distro $tunnel_interface $branch
+}
+
+function ceph_config {
+  echo "node: $(hostname -s)"
+  echo "storage_interface: $tunnel_interface"
+  echo "initial_member: $(hostname -s)"
+  echo "initial_mon: $(hostname -s)"
+  echo "ceph:"
+  echo "  monitors:"
+  echo "  - $IP"
+}
+
+general_config > /tmp/general_config.yaml
+ceph_config > /tmp/ceph_config.yaml
+
+helm upgrade test-ceph-mon-daemonset kolla/test-ceph-mon-daemonset --version $VERSION \
+    --namespace kolla \
+    --values /tmp/general_config.yaml \
+    --values /tmp/ceph_config.yaml
+
+$DIR/tools/wait_for_pods.sh kolla
+wait_for_ceph_bootstrap kolla
+
+helm upgrade ceph-osds0 kolla/test-ceph-osd-pod --version $VERSION \
+    --namespace kolla \
+    --values /tmp/general_config.yaml \
+    --values /tmp/ceph_config.yaml \
+    --set index=0
+helm upgrade ceph-osds1 kolla/test-ceph-osd-pod --version $VERSION \
+    --namespace kolla \
+    --values /tmp/general_config.yaml \
+    --values /tmp/ceph_config.yaml \
+    --set index=1
+
+$DIR/tools/wait_for_pods.sh kolla
+
+kubectl exec ceph-osd0 -c main --namespace=kolla -- /bin/bash -c \
+    "ceph -s"
+
