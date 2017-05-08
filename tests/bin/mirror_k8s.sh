0ec438016dde30ca6f28506b04aac04a0f43fa0a
Kevin Fox <kevin@efox.cc>
2017-05-08 06:52:43 -0700

    WIP: Cache k8s
    This PS caches the latest working Kubernetes software to insulate
    the gate from sudden changes in the released binaries.
    
    Change-Id: I2bd79073c66da24e57bf57a4e49ae2281a90b4df

---
+++ tests/bin/mirror_k8s.sh
@@ -0,0 +1,14 @@
+LOGS="$1"
+DISTRO="$2"
+TYPE="$3"
+CONFIG="$4"
+BRANCH="$5"
+PIPELINE="$6"
+
+if [ "x$DISTRO" == "xcentos" ]; then
+    sudo ls '/var/cache/yum/x86_64/7/*/packages/'
+fi
+
+if [ "x$PIPELINE" == "xperiodic" -a "x$DISTRO" == "xcentos" ]; then
+    echo
+fi
