6212d175f65e9d2a80cf58198c8c4aa7be8a7bdd
Steven Dake <stdake@cisco.com>
2017-05-11 11:45:31 -0400

    Kevins' line of debug
    This workflow deletes all of the configmaps, secrets, and helm charts
    within the kolla namespace.  This workflow further unlabels all nodes.
    
    Change-Id: I83a1a56d02ae64c845300fe2a0e4a1130f09a27e

---
+++ tests/bin/destroy_tests.sh
@@ -0,0 +1,40 @@
+#!/bin/bash -xe
+
+# Destroy kolla-kubernetes deployment and validate kolla-kubernetes is
+# indeed destroyed
+
+echo "a"
+. .venv/bin/activate
+echo "b"
+ansible-playbook -e ansible_python_interpreter=/usr/bin/python ansible/destroy.yml
+echo "c"
+deactivate
+echo "d"
+
+# Validate the deployment was indeed deleted
+echo "e"
+NAMESPACED_OBJECTS=`kubectl get all -n kolla -o name | wc -l`
+echo "f"
+LABELED_NODE_OBJECTS=`kubectl get nodes -o name --show-labels | grep kolla | wc -l`
+echo "g"
+PV_OBJECTS=`kubectl get pv | wc -l`
+echo "h"
+
+# If namespaced objects still exist, exit with failure
+if [ $NAMESPACED_OBJECTS -eq 0 ]; then
+echo "i"
+    exit 1
+fi
+
+# If labeled nodes still exist, exit with failure
+if [ $LABELED_NODE_OBJECTS -eq 0 ]; then
+echo "j"
+    exit 1
+fi
+
+# If PVs exist, exit with failure
+if [ $PV_OBJECTS -eq 0 ]; then
+echo "k"
+    exit 1
+fi
+echo "l"
