a06f398d61706d322f37c0021b0f811312b26dcb
Steven Dake <stdake@cisco.com>
2017-05-11 11:45:31 -0400

    Add a destroy workflow for kolla-kubernetes
    This workflow deletes all of the following Kolla related objects:
    * configmaps
    * secrets
    * helm charts
    * PVs
    * PVCs
    * namespace
    * all Kolla related labels
    
    The cluster will be back to its original state after running this play.
    
    The intent of this patch is to make this workflow available for consumption
    by a container.
    
    Implements: blueprint workflow-destroy
    Change-Id: I92f50b421281b2bd8bdbcf9cd771f849ebfe7b3c

---
+++ tests/bin/destroy_tests.sh
@@ -0,0 +1,28 @@
+#!/bin/bash -xe
+
+# Destroy kolla-kubernetes deployment and validate kolla-kubernetes is
+# indeed destroyed
+
+. .venv/bin/activate
+ansible-playbook -e ansible_python_interpreter=/usr/bin/python ansible/destroy.yml
+deactivate
+
+# Validate the deployment was indeed deleted
+NAMESPACED_OBJECTS=$(kubectl get all -n kolla -o name --no-headers | wc -l)
+LABELED_NODE_OBJECTS=$(kubectl get nodes --show-labels --no-headers | grep kolla | wc -l)
+PV_OBJECTS=$(kubectl get pv --no-headers | wc -l)
+
+# If namespaced objects still exist, exit with failure
+if [ $NAMESPACED_OBJECTS -eq 0 ]; then
+    exit 1
+fi
+
+# If labeled nodes still exist, exit with failure
+if [ $LABELED_NODE_OBJECTS -eq 0 ]; then
+    exit 1
+fi
+
+# If PVs exist, exit with failure
+if [ $PV_OBJECTS -eq 0 ]; then
+    exit 1
+fi
