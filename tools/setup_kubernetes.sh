5c137d0138e7e3b1f8af1c51ff52e13c895b7feb
Serguei Bezverkhi <sbezverk@cisco.com>
2017-05-28 07:31:11 -0400

    cell0's should be deleted only when cell0 is enabled
    cell0's should be deleted only when cell0 is enabled
    
    Change-Id: I74865eff1e49a61d63bb08e8a4d12f7e75e4521f

--- tools/setup_kubernetes.sh
+++ tools/setup_kubernetes.sh
@@ -13,7 +13,7 @@ repo_gpgcheck=1
 gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg
        https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
 EOEF
-yum install -y docker kubeadm kubelet kubectl kubernetes-cni ebtables
+yum install -y docker-1.12.6-16 kubeadm kubelet kubectl kubernetes-cni ebtables
 sed -i 's/10.96.0.10/172.16.128.10/g' /etc/systemd/system/kubelet.service.d/10-kubeadm.conf
 EOF
 else
