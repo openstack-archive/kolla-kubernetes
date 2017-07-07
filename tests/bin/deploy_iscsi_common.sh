9b0b2d8aa6ab9555b284633ae714f1cece456b8f
Serguei Bezverkhi <sbezverk@cisco.com>
2017-07-07 12:12:23 -0400

    WIP Adding lioadm iscsi_helper support to master
    Adding support for lioadm as iscsi_helper for iscsi lvm
    backend for cinder.
    
    Change-Id: I8beabd10755bb04c4819263e54170a780b879716

--- tests/bin/deploy_iscsi_common.sh
+++ tests/bin/deploy_iscsi_common.sh
@@ -136,7 +136,7 @@ helm install kolla/nova-novncproxy-svc --version $VERSION \
     --values /tmp/general_config.yaml --values /tmp/iscsi_config.yaml
 
 if [ "x$branch" != "x2" -a "x$branch" != "x3" ]; then
-helm install kolla/nova-placement-svc --debug --version $VERSION \
+helm install kolla/nova-placement-svc --version $VERSION \
     --namespace kolla --name nova-placement-svc \
     --values /tmp/general_config.yaml --values /tmp/iscsi_config.yaml
 fi
@@ -234,14 +234,14 @@ $DIR/tools/wait_for_pods.sh kolla
 #
 sudo ifconfig br-ex up
 
-helm install kolla/keepalived-daemonset --debug --version $VERSION \
+helm install kolla/keepalived-daemonset --version $VERSION \
     --namespace kolla --name keepalived-daemonset \
     --values /tmp/general_config.yaml --values /tmp/iscsi_config.yaml
 
 $DIR/tools/wait_for_pods.sh kolla
 
 if [ "x$branch" != "x2" -a "x$branch" != "x3" ]; then
-helm install kolla/nova-placement-create-keystone-service-job --debug --version $VERSION \
+helm install kolla/nova-placement-create-keystone-service-job --version $VERSION \
     --namespace kolla --name nova-placement-create-keystone-service \
     --values /tmp/general_config.yaml --values /tmp/iscsi_config.yaml
 fi
@@ -269,7 +269,7 @@ helm install kolla/cinder-create-keystone-service-job --version $VERSION \
     --values /tmp/general_config.yaml --values /tmp/iscsi_config.yaml
 
 if [ "x$branch" != "x2" -a "x$branch" != "x3" ]; then
-helm install kolla/nova-placement-create-keystone-user-job --debug --version $VERSION \
+helm install kolla/nova-placement-create-keystone-user-job --version $VERSION \
     --namespace kolla --name nova-placement-create-keystone-user \
     --values /tmp/general_config.yaml --values /tmp/iscsi_config.yaml
 fi
@@ -315,7 +315,7 @@ helm install kolla/nova-create-keystone-endpoint-public-job --version $VERSION \
     --values /tmp/general_config.yaml --values /tmp/iscsi_config.yaml
 
 if [ "x$branch" != "x2" -a "x$branch" != "x3" ]; then
-helm install kolla/nova-placement-create-keystone-endpoint-public-job --debug --version $VERSION \
+helm install kolla/nova-placement-create-keystone-endpoint-public-job --version $VERSION \
     --namespace kolla --name nova-placement-create-keystone-endpoint-public \
     --values /tmp/general_config.yaml --values /tmp/iscsi_config.yaml
 fi
@@ -355,11 +355,11 @@ helm install kolla/cinder-manage-db-job --version $VERSION \
     --values /tmp/general_config.yaml --values /tmp/iscsi_config.yaml
 
 if [ "x$branch" != "x2" -a "x$branch" != "x3" ]; then
-helm install kolla/nova-placement-create-keystone-endpoint-internal-job --debug --version $VERSION \
+helm install kolla/nova-placement-create-keystone-endpoint-internal-job --version $VERSION \
     --namespace kolla --name nova-placement-create-keystone-endpoint-internal \
     --values /tmp/general_config.yaml --values /tmp/iscsi_config.yaml
 
-helm install kolla/nova-placement-create-keystone-endpoint-admin-job --debug --version $VERSION \
+helm install kolla/nova-placement-create-keystone-endpoint-admin-job --version $VERSION \
     --namespace kolla --name nova-placement-create-keystone-endpoint-admin \
     --values /tmp/general_config.yaml --values /tmp/iscsi_config.yaml
 fi
@@ -472,7 +472,7 @@ for x in nova-api nova-novncproxy; do
 done
 
 if [ "x$branch" != "x2" -a "x$branch" != "x3" ]; then
-helm install kolla/nova-placement-deployment --debug --version $VERSION \
+helm install kolla/nova-placement-deployment --version $VERSION \
     --namespace kolla --name nova-placement-deployment \
     --values /tmp/general_config.yaml --values /tmp/iscsi_config.yaml
 fi
@@ -525,13 +525,22 @@ helm install kolla/iscsid-daemonset --version $VERSION \
     --namespace kolla --name iscsid-daemonset \
     --values /tmp/general_config.yaml --values /tmp/iscsi_config.yaml
 
-helm install kolla/tgtd-daemonset --version $VERSION \
-    --namespace kolla --name tgtd-daemonset \
-    --values /tmp/general_config.yaml --values /tmp/iscsi_config.yaml
+if [ "x$branch" != "x2" -a "x$branch" != "x3" -a "x$branch" != "x4" ]; then
+  helm install kolla/iscsi-target-daemonset --version $VERSION  \
+      --namespace kolla --name iscsi-target-daemonset \
+      --values /tmp/general_config.yaml --values /tmp/iscsi_config.yaml
+else
+  helm install kolla/tgtd-daemonset --version $VERSION \
+      --namespace kolla --name tgtd-daemonset \
+      --values /tmp/general_config.yaml --values /tmp/iscsi_config.yaml
+fi
 
 $DIR/tools/pull_containers.sh kolla
 $DIR/tools/wait_for_pods.sh kolla
 $DIR/tools/build_local_admin_keystonerc.sh
+
+kubectl get pods -n kolla
+
 . ~/keystonerc_admin
 
 #
