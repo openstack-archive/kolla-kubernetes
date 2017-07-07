2c36e2f5cc1b2d1bcf70902a4c3a35ee10d1a8e8
Serguei Bezverkhi <sbezverk@cisco.com>
2017-07-07 12:12:23 -0400

    WIP Adding alternative iscsi_helper support
    This is first step in adding support for lioadm as iscsi_helper for
    iscsi lvm backend for cinder.
    
    Change-Id: I8beabd10755bb04c4819263e54170a780b879716

--- tests/bin/common_iscsi_config.sh
+++ tests/bin/common_iscsi_config.sh
@@ -3,6 +3,7 @@ function common_iscsi_config {
     echo "  kolla:"
     echo "    all:"
     echo "      storage_provider: host"
+    echo "      storage_interface: docker0"
     echo "      storage_provider_fstype: xfs"
     echo "      ceph_backend: false"
     echo "      lvm_backends:"
