860752d373409e83ab3b73c4879ac1deb34d83ed
Serguei Bezverkhi <sbezverk@cisco.com>
2017-07-07 12:12:23 -0400

    Adding lioadm iscsi_helper support to master
    Adding support for lioadm as iscsi_helper for iscsi lvm
    backend for cinder and converting gate job for master branch
    to use lioadm helper instead of tgtadm.
    
    Change-Id: I8beabd10755bb04c4819263e54170a780b879716

--- tools/setup_registry.sh
+++ tools/setup_registry.sh
@@ -12,11 +12,19 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/" && pwd )"
 
 echo "Deploying registry for: $Distro - $Type - $Branch"
 
+#
+# Attempting to use reverse proxy for tarball download.
+#
+if [ -f /etc/ci/mirror_info.sh ]; then
+   sudo source /etc/ci/mirror_info.sh
+   echo "Suggested proxy url: $NODEPOOL_TARBALLS_PROXY"
+fi
+
 helm install kolla/registry-deployment --version $VERSION --debug \
              --namespace kolla --name registry \
              --set initial_load=true --set node_port=30401 \
              --set distro=$Distro --set type=$Type
 
-$DIR/wait_for_pods.sh kolla 600
+$DIR/wait_for_pods.sh kolla 900
 
 echo "Registry with images for: $Distro - $Type - $Branch is running..."
