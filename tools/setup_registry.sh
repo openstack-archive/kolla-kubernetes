77da5151dd8ccc54624e04702c80a96e27e142c2
Serguei Bezverkhi <sbezverk@cisco.com>
2017-07-14 14:30:38 -0400

    Adding chart for docker registry and serving master images
    Docker registry will be running as a pod, if configured, it will load
    tarball with images and start serviceg then inside of a kubernetes cluster.
    
    Change-Id: I9b0df3c27148b22b204403a38e8b49495e31e03f

---
+++ tools/setup_registry.sh
@@ -0,0 +1,37 @@
+#!/bin/bash -e
+#
+#  Passed parameters: $1 - Distro, $2 - Type,
+#                     $3 - Branch
+#
+Distro="$1"
+Type="$2"
+Branch="$3"
+VERSION=0.7.0-1
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/" && pwd )"
+
+echo "Deploying registry for: $Distro - $Type - $Branch"
+#
+# Just for debugging dry run
+#
+pwd
+ls -l
+ls -l $DIR/../
+helm install $DIR/../helm/microservice/registry-deployment --debug --dry-run \
+             --namespace kolla --name registry \
+             --set initial_load=true --set node_port=30401 \
+             --set distro=$Distro --set type=$Type \
+             --values $DIR/../helm/microservice/registry-deployment/values.yaml
+#
+#
+#
+
+helm install $DIR/../helm/microservice/registry-deployment --debug \
+             --namespace kolla --name registry \
+             --set initial_load=true --set node_port=30401 \
+             --set distro=$Distro --set type=$Type \
+             --values $DIR/../helm/microservice/registry-deployment/values.yaml
+
+$DIR/wait_for_pods.sh kolla 600
+
+echo "Registry with images for: $Distro - $Type - $Branch is running..."
