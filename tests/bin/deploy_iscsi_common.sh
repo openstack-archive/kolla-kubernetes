a7accd31d829e173b376458ac84779c70f9ca964
Serguei Bezverkhi <sbezverk@cisco.com>
2017-07-14 14:30:38 -0400

    WIP Adding chart for docker registry
    Docker registry will be running as a pod, if configured, it will load
    tarball with images and start serviceg then inside of a kubernetes cluster.
    
    Change-Id: I9b0df3c27148b22b204403a38e8b49495e31e03f

--- tests/bin/deploy_iscsi_common.sh
+++ tests/bin/deploy_iscsi_common.sh
@@ -70,6 +70,19 @@ fi
 general_config $IP $base_distro $tunnel_interface $branch > /tmp/general_config.yaml
 iscsi_config > /tmp/iscsi_config.yaml
 
+#
+# NOTE(sbezverk) WIP when completed will be moved to a common to all master jobs
+# location.
+#
+helm install kolla/registry-deployment --version $VERSION --debug \
+             --namespace kolla --name registry \
+             --set initial_load=true --set node_port=30401 
+$DIR/tools/wait_for_pods.sh kolla 600
+
+sudo docker images
+sudo docker pull 127.0.0.1:30401/lokolla/centos-source-iscsid:5.0.0
+sudo docker images
+
 for x in mariadb rabbitmq glance; do
     helm install kolla/$x-pv --version $VERSION \
         --name $x-pv --values /tmp/general_config.yaml --values /tmp/iscsi_config.yaml
