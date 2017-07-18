2bfc840642b5ebf6e21552ca445058aefd70971c
Kevin Fox <kevin@efox.cc>
2017-07-18 06:54:29 -0700

    Helm Native Values Step 1.
    This PS starts converting away from generating microservices
    values.yaml to building it using the helm native parent
    values feature.
    
    Change-Id: Ieac62538662fc267bb42e2c795a05bdd8d8e3120

--- kolla_kubernetes/tests/test_helm_templates.py
+++ kolla_kubernetes/tests/test_helm_templates.py
@@ -48,6 +48,11 @@ class TestK8sTemplatesTest(base.BaseTestCase):
             if err:
                 raise err
 
+            #FIXME
+            if package == 'iscsi-target-daemonset':
+              print out.readlines()
+              raise ("ERROR")
+
             l = yaml.safe_load_all(out)
             for y in l:
                 js = '[]'
