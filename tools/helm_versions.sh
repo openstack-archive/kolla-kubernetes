e9c210fbce5d5f01fcaf85c7e339ae19624aec2f
Kevin Fox <kevin@efox.cc>
2017-07-18 06:54:29 -0700

    Helm Native Values Step 1.
    This PS starts converting away from generating microservices
    values.yaml to building it using the helm native parent
    values feature.
    
    Change-Id: Ieac62538662fc267bb42e2c795a05bdd8d8e3120

--- tools/helm_versions.sh
+++ tools/helm_versions.sh
@@ -1,4 +1,3 @@
-HELM_VERSION="2.4.2"
-HELM_TEMPLATE_URL="https://github.com/technosophos/helm-template/releases/download/2.2.2%2B1/helm-template-linux-2.2.2.1.tgz"
+HELM_VERSION="2.5.1"
 
 HELM_URL="http://storage.googleapis.com/kubernetes-helm/helm-v$HELM_VERSION-linux-amd64.tar.gz"
