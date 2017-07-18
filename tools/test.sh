b86aac506ad7205fe97039c0cc456f25edb5b1b2
Kevin Fox <kevin@efox.cc>
2017-07-18 06:54:29 -0700

    Helm Native Values Step 1.
    This PS starts converting away from generating microservices
    values.yaml to building it using the helm native parent
    values feature.
    
    Change-Id: Ieac62538662fc267bb42e2c795a05bdd8d8e3120

--- tools/test.sh
+++ tools/test.sh
@@ -10,12 +10,12 @@ grep orchestration_engine etc/kolla/globals.yml || echo orchestration_engine: KU
 #tools/build_helm_templates.sh
 set -x
 . tools/helm_versions.sh
-mkdir -p ~/.helm/plugins/template
-curl -L -o /tmp/helm-template.tar.gz "$HELM_TEMPLATE_URL"
-curl -L -o /tmp/helm.tar.gz "$HELM_URL"
+#FIXME override the helm version to get an unreleased version with template support built in.
+HELM_URL="https://github.com/jascott1/bins/raw/master/helm/v2.6%2Be8fb5035/linux-amd64/helm"
+curl -L -o ~/bin/helm "$HELM_URL"
+#curl -L -o /tmp/helm.tar.gz "$HELM_URL"
 mkdir -p ~/bin
-tar --strip-components 1 -C ~/bin linux-amd64/helm -zxf /tmp/helm.tar.gz
-tar -C ~/.helm/plugins/template/ -zxf /tmp/helm-template.tar.gz
+#tar --strip-components 1 -C ~/bin linux-amd64/helm -zxf /tmp/helm.tar.gz
 export PATH=$PATH:~/bin
 export HOME=$(cd ~; pwd)
 export HELMDIR=$(pwd)/helm
@@ -23,6 +23,5 @@ export HELMBIN=$HOME/bin/helm
 export HELM_HOME=$HOME/.helm/
 export REPODIR=/tmp/repo.$$
 helm init -c
-helm template || true
 tools/helm_build_all.sh /tmp/repo.$$
 python setup.py test --slowest --testr-args="$1"
