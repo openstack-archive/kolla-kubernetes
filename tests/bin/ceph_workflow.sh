d4c3cf8f730ca625f7fdb4286bc7d7ca74c1ffe8
Kevin Fox <kevin@efox.cc>
2017-08-14 12:37:01 -0700

    WIP: Initial stab at ceph dynamic provisioning support.
    Change-Id: Ie40b5b8393fe0f28641f5ee7301249cb6e4d6d2d

--- tests/bin/ceph_workflow.sh
+++ tests/bin/ceph_workflow.sh
@@ -33,8 +33,8 @@ general_config > /tmp/general_config.yaml
 ceph_config > /tmp/ceph_config.yaml
 
 for x in mariadb rabbitmq glance helm-repo; do
-    helm install kolla/$x-pv --version $VERSION \
-        --name $x-pv --values /tmp/general_config.yaml --values /tmp/ceph_config.yaml
+#    helm install kolla/$x-pv --version $VERSION \
+#        --name $x-pv --values /tmp/general_config.yaml --values /tmp/ceph_config.yaml
     helm install kolla/$x-pvc --version $VERSION --namespace kolla \
         --name $x-pvc --values /tmp/general_config.yaml --values /tmp/ceph_config.yaml
 done
@@ -43,9 +43,9 @@ helm install kolla/helm-repo-svc --version $VERSION \
     --namespace kolla --name helm-repo-svc --set element_name=helm-repo \
     --values /tmp/general_config.yaml --values /tmp/ceph_config.yaml
 
-helm install kolla/helm-repo-deployment --version $VERSION \
-    --namespace kolla --name helm-repo-deployment --set "element_name=helm-repo" \
-    --values /tmp/general_config.yaml --values /tmp/ceph_config.yaml
+#helm install kolla/helm-repo-deployment --version $VERSION \
+#    --namespace kolla --name helm-repo-deployment --set "element_name=helm-repo" \
+#    --values /tmp/general_config.yaml --values /tmp/ceph_config.yaml
 
 helm install kolla/memcached-svc --version $VERSION \
     --namespace kolla --name memcached-svc \
@@ -137,7 +137,7 @@ helm install kolla/rabbitmq-init-element-job --version $VERSION \
     --values /tmp/general_config.yaml --values /tmp/ceph_config.yaml
 
 $DIR/tools/pull_containers.sh kolla
-$DIR/tools/wait_for_pods.sh kolla
+$DIR/tools/wait_for_pods.sh kolla 240
 
 for x in mariadb rabbitmq; do
     helm delete $x-init-element-job --purge
