fed687956920036fc7d26c3ca9c0318f477bb709
rwellum <rwellum@lenovo.com>
2018-01-03 16:17:15 -0500

    WIP: Do not merge - testing current state of kolla-kubernetes
    Change-Id: I6b771cb49c75b1e0e435e4daef885bbe30bad2e7

--- kolla_kubernetes/commands/genpwd.py
+++ kolla_kubernetes/commands/genpwd.py
@@ -12,6 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# Comment to foce zuul to test
+
 import argparse
 import hmac
 import os
